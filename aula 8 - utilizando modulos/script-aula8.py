d22d0f9e13c3fb57a818429f5a3a330d725af38c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-01 18:12:35 -0300

    link biblioteca

--- aula 8 - utilizando modulos/script-aula8.py
+++ aula 8 - utilizando modulos/script-aula8.py
@@ -18,6 +18,7 @@ print(random.randint(1, 60), random.randint(1, 60), random.randint(1, 60),
       random.randint(1, 60),random.randint(1, 60), random.randint(1, 60))
 
 #https://www.webfx.com/tools/emoji-cheat-sheet/
+#https://pypi.org/project/emoji/
 import emoji
 print(emoji.emojize('Olá mundo :earth_americas:'))
 print(emoji.emojize('Python is :winking_face:'))#O nome do codigo é shortcode.
