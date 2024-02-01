8a1fe6ce1c5e29f961ad71e988844355424360e5
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-01 18:09:37 -0300

    aula 8 - Utilizando modulos (parte 1)

---
+++ aula 8 - utilizando modulos/script-aula8.py
@@ -0,0 +1,23 @@
+#Para ver todas as bibliotecas, colocamos "import" e apertamos ctrl + espaço
+#import math > Nós importamos toda a biblioteca math
+#from math import sqrt > Importamos somente sqrt da biblioteca de matematica
+#from math import sqrt, ceil > Importando duas funcionalidades da biblioteca math
+
+"""import math
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, math.ceil(raiz)))
+
+from math import sqrt, ceil
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, ceil(raiz)))"""
+
+import random
+print(random.randint(1, 60), random.randint(1, 60), random.randint(1, 60),
+      random.randint(1, 60),random.randint(1, 60), random.randint(1, 60))
+
+#https://www.webfx.com/tools/emoji-cheat-sheet/
+import emoji
+print(emoji.emojize('Olá mundo :earth_americas:'))
+print(emoji.emojize('Python is :winking_face:'))#O nome do codigo é shortcode.
