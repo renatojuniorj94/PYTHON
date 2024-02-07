56a0a1482dd15e63393fa728de719472a76b2174
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-07 00:23:06 -0300

    correção parte 2

--- aula 8 - utilizando modulos/desafio19-aula8.py
+++ aula 8 - utilizando modulos/desafio19-aula8.py
@@ -8,4 +8,6 @@ a1 = input('Primeiro aluno: ')
 a2 = input('Segundo aluno: ')
 a3 = input('Terceiro aluno: ')
 a4 = input('Quarto aluno: ')
-print('')
+lista = [a1, a2, a3, a4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
--- aula 8 - utilizando modulos/desafio21-aula8.py
+++ aula 8 - utilizando modulos/desafio21-aula8.py
@@ -3,3 +3,9 @@ Faça um programa em python que abra e reproduza o áudio de
 um arquivo MP3.
 """
 
+import pygame
+pygame.init()
+pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play()
+input()
+pygame.event.wait()
