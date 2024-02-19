57d8e8ba103a11c0cf372796e4423369f306fd38
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-19 10:03:05 -0300

    corrigindo ex034

--- aula 10 - condições/desafio34-aula10.py
+++ aula 10 - condições/desafio34-aula10.py
@@ -8,3 +8,14 @@ if s <= 1250:
     print('Seu aumento vai ser de R${}, e o seu novo salário é de R$ {}'.format(s * 15 / 100, s + (s * 15 / 100)))
 else:
     print('Seu aumento vai ser de R$ {}, e o seu novo salário é de R$ {}'.format(s * 10 / 100, s + (s * 10 / 100)))
+
+#Correto! :D
+
+#Outra maneira de resolver...
+
+s = float(input('Digite o seu salário: R$ '))
+if s <= 1250:
+    novo = s + (s * 15 / 100)
+else:
+    novo = s + (s * 10 / 100)
+print('Quem ganhava R$ {:.2f} passa a ganhar R$ {:.2f} agora.'.format(s, novo))
