bb110bb2793492cb0cd7352a873021bb00ab6ca6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-18 23:42:03 -0300

    corrigindo ex029

--- aula 10 - condições/desafio29-aula10.py
+++ aula 10 - condições/desafio29-aula10.py
@@ -3,9 +3,19 @@ Escreva um programa que leia a velocidade de um carro.
 Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado.
 A multa vai custar R$ 7 por cada km acima do limite.
 '''
-vel = int(input('Digite a velocidade do veiculo: '))
+vel = float(input('Digite a velocidade do veiculo: '))
 if vel <= 80:
     print('Sua velocidade é {}Km/h, Dirija com segurança e boa viagem!'.format(vel))
 else:
+    print('VOCÊ FOI MULTADO! Sua velocidade é de {}Km/h, é o valor da sua multa é de R$ {}'.format(vel, (vel - 80) * 7))
 
-    print('VOCÊ FOI MULTADO! Sua velocidade é de {}Km/h, é o valor da sua multa é de R$ {}'.format(vel, vel > 1 * 7))
+# Correto! :D
+
+# Outra maneira de fazer usando uma condição simples.
+
+velocidade = float(input('Qual é a velocidade atual do carro? '))
+if velocidade >= 80:
+    print('MULTADO! Você excedeu o limite de velocidade permitido que é de 80km/h')
+    multa = (velocidade - 80) * 7
+    print('Você deve pagar uma multa de R${:.2f}'.format(multa))
+    print('Tenha um bom dia! Dirija com segurança!')
