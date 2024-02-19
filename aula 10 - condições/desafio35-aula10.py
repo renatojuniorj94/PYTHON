8c996b33c7b6092eec866e69b829a1766aab000b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-19 10:36:16 -0300

    correção ex035

--- aula 10 - condições/desafio35-aula10.py
+++ aula 10 - condições/desafio35-aula10.py
@@ -2,7 +2,15 @@
 Desenvolva um programa que leia o comprimento de três retas e diga
 ao usuário se elas podem ou não formar um triângulo.
 '''
-print('Diga o comprimento de 3 retas')
-r1 = float(input('Reta 1: '))
-r2 = float(input('Reta 2: '))
-r3 = float(input('Reta 3: '))
+
+print('-=-' * 10)
+print('Analisado de triângulos')
+print('-=-' * 10)
+r1 = float(input('Primeiro seguimento: '))
+r2 = float(input('Segundo seguimento: '))
+r3 = float(input('Terceiro seguimento: '))
+
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os seguimentos acima PODEM FORMAR triângulo!')
+else:
+    print('Os seguimentos acima NÃO PODEM FORMAR triângulos!')
