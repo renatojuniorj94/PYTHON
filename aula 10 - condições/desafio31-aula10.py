4ff3fc66d41d7c17a002b89b997097da30c66e95
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-19 07:57:07 -0300

    corrigindo ex031

--- aula 10 - condições/desafio31-aula10.py
+++ aula 10 - condições/desafio31-aula10.py
@@ -8,3 +8,22 @@ if km <= 200:
     print('O valor da passagem é de R$ {:.2f}'.format(km * 0.50))
 else:
     print('O valor da passagem é de R$ {:.2f}'.format(km * 0.45))
+
+#Cooreto! :D
+
+#Outra maneira de fazer...
+distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
+if distancia <= 200:
+    preço = distancia * 0.50
+else:
+    preço = distancia * 0.45
+print('E o preço da sua passagem será de R$ {:.2f}'.format(preço))
+
+#Usando if inline ou operador ternário:
+'''
+distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {}Km.'.format(distancia))
+valor = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print('E o preço da sua passagem será de R$ {:.2f}'.format(preço))
+'''
