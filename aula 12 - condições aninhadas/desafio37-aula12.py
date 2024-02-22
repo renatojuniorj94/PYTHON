954b84aac725889162c38e0a0532c441b3795253
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-22 20:17:52 -0300

    corrigindo desafio 37

--- aula 12 - condições aninhadas/desafio37-aula12.py
+++ aula 12 - condições aninhadas/desafio37-aula12.py
@@ -7,16 +7,18 @@ qual será a base de conversão:
 '''
 
 numero = int(input('Digite um número inteiro: '))
-print('1 - Binário\n'
-      '2 - Octal\n'
-      '3 - Hexadecimal')
-base = int(input('Escolha a base de conversão: '))
+print('''1 - Binário
+2 - Octal
+3 - Hexadecimal''')
+opção = int(input('Escolha a base de conversão: '))
 
-if base == 1:
-    print('Você escolheu binário como base.')
-elif base == 2:
-    print('Você escolheu Octal como base.')
-elif base == 3:
-    print('Você escolheu Hexadecimal como base.')
+if opção == 1:
+    print('{} convertido para BINÁRIO é igual a {}.'.format(numero, bin(numero) [2:]))
+elif opção == 2:
+    print('{} convertido para OCTAL é igual {}.'.format(numero, oct(numero) [2:]))
+elif opção == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}.'.format(numero, hex(numero) [2:]))
 else:
     print('\033[1;31mErro!\033[m Número inválido!')
+
+# Correto! :D
