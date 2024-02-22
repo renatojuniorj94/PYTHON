3e1e9695c070a2655759a75d1714b851ba5bbbbd
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-22 19:54:46 -0300

    corrigindo desafio 36

--- aula 12 - condições aninhadas/deafio36-aula12.py
+++ aula 12 - condições aninhadas/deafio36-aula12.py
@@ -5,15 +5,18 @@ Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do 
 será negado.
 '''
 
-casa = float(input('Qual o valor do imóvel? '))
-salário = float(input('Digite o seu salário '))
+casa = float(input('Qual o valor do imóvel? R$ '))
+salário = float(input('Digite o seu salário: R$ '))
 anos = int(input('Em quanto anos você vai pagar o imóvel? '))
 
 mensalidade = casa / (anos * 12)
-trinta_porcento = salário / 100 * 30
+trinta_porcento = salário / 100 * 30 # Ou salário * 30 / 100
 
 if mensalidade > trinta_porcento:
     print('O valor da parcela ficou em R$ \033[31m{:.2f}\033[m em {} meses, e \033[31minfelizmente\033[m não será possível seguir com o financiamento.'.format(mensalidade, anos * 12))
 else:
-    print('\033[32mParabéns!\033[m Sua renda foi aprovada e podemos seguir com a negociação.'
-          ' O valor da parcela será de R$ \033[32m{:.2f}\033[m em \033[32m{}\033[m meses.'.format(mensalidade, anos * 12))
+    print('\033[32mParabéns\033[m! Sua renda foi aprovada e podemos seguir com a negociação.\n'
+          'O valor da parcela será de R$ \033[32m{:.2f}\033[m em \033[32m{}\033[m meses.'.format(mensalidade, anos * 12))
+
+
+# Correto! :D
