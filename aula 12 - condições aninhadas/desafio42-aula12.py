4845328bf4f032687e33cc3f3ed46a07fb8b3fe7
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-27 15:08:40 -0300

    corrigindo desafio 42

--- aula 12 - condições aninhadas/desafio42-aula12.py
+++ aula 12 - condições aninhadas/desafio42-aula12.py
@@ -14,6 +14,7 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os seguimentos acima PODEM FORMAR triângulo!')
 else:
     print('Os seguimentos acima NÃO PODEM FORMAR triângulos!')
+    exit()
 
 if r1 == r2 == r3:
     print('Nós temos um triângulo equilátero.')
@@ -21,3 +22,5 @@ elif r1 == r2 != r3 or r1 != r2 == r3 or r1 == r3 != r2:
     print('Nós temos um triângulo isósceles.')
 elif r1 != r2 != r3:
     print('Nós temos um triângulo escaleno.')
+
+# Correto! :D
