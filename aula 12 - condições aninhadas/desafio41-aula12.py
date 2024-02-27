b78540862ed0c7bdf75b39daf4c4b2564f7dd36f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-02-27 14:47:58 -0300

    corrigindo desafio 41

--- aula 12 - condições aninhadas/desafio41-aula12.py
+++ aula 12 - condições aninhadas/desafio41-aula12.py
@@ -11,9 +11,9 @@ from datetime import date
 ano = int(input('Digite o ano de nascimento do atleta: '))
 idade = date.today().year - ano
 
-if 0 < idade <= 1:
+if 0 == idade <= 1:
     print('O atleta tem {} ano e pertence a categoria MIRIM.'.format(idade))
-elif 1 < idade == 9:
+elif 1 < idade <= 9:
     print('O atleta tem {} anos e pertence a categoria MIRIM.'.format(idade))
 elif 10 <= idade <= 14:
     print('O atleta tem {} anos e pertence a categoria INFANTIL.'.format(idade))
@@ -21,3 +21,5 @@ elif 15 <= idade <= 19:
     print('O atleta tem {} anos e pertence a categoria JUNIOR.'.format(idade))
 else:
     print('O atleta tem {} anos e pertence a categoria MASTER.'.format(idade))
+
+# Correto! :D
