a9fea8864345604f6f575e598e3bc81e342db1f5
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-03-03 00:13:43 -0300

    corrigindo aula 44

--- aula 12 - condições aninhadas/desafio44-aula12.py
+++ aula 12 - condições aninhadas/desafio44-aula12.py
@@ -13,8 +13,8 @@ form_pag = int(input('Digite a forma de pagamento: \n'
                      '2 - Á vista no cartão de crédito ou débito (5% de desconto)\n'
                      '3 - Em até 2x no cartão de crédito\n'
                      '4 - 3x ou mais no cartão de crédito (20% de juros): '))
-vista = preco / 100 * 90
-vista_cartao = preco / 100 * 95
+vista = preco / 100 * 90 # Ou preco - (preco * 10 / 100)
+vista_cartao = preco / 100 * 95 # Ou preco - (preco * 5 / 100)
 cartao_tres = preco / 100 * 120
 
 if form_pag == 1:
@@ -29,3 +29,4 @@ else:
     print('\033[1;31mOpção inválida!')
 
 # Correto! :D
+# Refazer essa correção!
