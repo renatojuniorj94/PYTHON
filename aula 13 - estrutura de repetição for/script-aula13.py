ddc09f87488a5649c5ab239cadda6b63f6762409
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-03-19 18:17:23 -0300

    script aula 13

--- aula 13 - estrutura de repetição for/script-aula13.py
+++ aula 13 - estrutura de repetição for/script-aula13.py
@@ -1,3 +1,28 @@
 """
-condição
+for c in range(1, 6):
+    print(c)
+print('FIM')
 """
+
+#Contando de trás para frente:
+for c in range(6, 0, -1):
+    print(c)
+print('Fim\n')
+
+#Pulando números:
+for p in range(0, 11, 2):
+    print(p)
+
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('FIM')
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    # Versão extensa: s = s + n
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
