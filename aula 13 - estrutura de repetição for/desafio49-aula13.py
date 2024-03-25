4b9ed850e31982ceba19fb90444102911b615910
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-03-25 20:48:30 -0300

    desafio 49

--- aula 13 - estrutura de repetição for/desafio49-aula13.py
+++ aula 13 - estrutura de repetição for/desafio49-aula13.py
@@ -2,3 +2,8 @@
 Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher,
 só que agora utilizando um laço for.
 '''
+
+n = int(input('Digite um número inteiro: '))
+
+for c in range (1, 11):
+    print('{} x {} = {}'.format(n, c, n * c))
