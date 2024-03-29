14523b812ac8111b0ba87caf9ef239a2e788237b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-03-29 00:04:20 -0300

    concluindo desafio 48

--- aula 13 - estrutura de repetição for/desafio48-aula13.py
+++ aula 13 - estrutura de repetição for/desafio48-aula13.py
@@ -7,6 +7,8 @@ e que se encontram no intervalo de 1 até 500.
 https://docs.python.org/pt-br/3/library/functions.html#sum
 '''
 
+print('A seguir são os número ímpares múltiplos de 3, do 1 ao 500:\n')
 for i in range(3, 501, 6):
-    '''print(i)'''
-    print(sum(range(3, 501, 6)))
+    print(i)
+soma = sum(range(3, 501, 6))
+print('\nO resultado soma foi entre todos os números ímpares, que são múltiplos de 3 foi: {}'.format(soma))
