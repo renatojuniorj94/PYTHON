df004060dcea72ad2a0f219f6939e17399ac925f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-03-28 23:55:06 -0300

    desafio 48
    incluindo link da documentação, sum()

--- aula 13 - estrutura de repetição for/desafio48-aula13.py
+++ aula 13 - estrutura de repetição for/desafio48-aula13.py
@@ -3,6 +3,10 @@ Faça um programa que calcule a soma entre todos os números ímpares que são m
 e que se encontram no intervalo de 1 até 500.
 '''
 
+'''
+https://docs.python.org/pt-br/3/library/functions.html#sum
+'''
+
 for i in range(3, 501, 6):
     '''print(i)'''
     print(sum(range(3, 501, 6)))
