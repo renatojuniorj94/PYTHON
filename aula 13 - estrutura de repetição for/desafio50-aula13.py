936cf56002c30e8bdad4ee4447e508a4eaa32160
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-03-30 23:50:42 -0300

    desafio 50

--- aula 13 - estrutura de repetição for/desafio50-aula13.py
+++ aula 13 - estrutura de repetição for/desafio50-aula13.py
@@ -3,6 +3,8 @@ Desenvolva um programa que leia seis números inteiros e mostre apenas daqueles
 que forem pares. Se o valor digitado for ímpar, desconsidero-o.
 '''
 
-for c in range(0, 4):
+for c in range(0, 6):
     n = int(input('Digite um número inteiro: '))
+'''qtd = len([c for c in range(10) if c % 2 == 0])'''
+
 
