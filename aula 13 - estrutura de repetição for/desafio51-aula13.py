d74352f6083b8341e0fd339ec990e9f27a27225e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-02 18:56:19 -0300

    desafio 51

--- aula 13 - estrutura de repetição for/desafio51-aula13.py
+++ aula 13 - estrutura de repetição for/desafio51-aula13.py
@@ -2,3 +2,9 @@
 Desenvolva um programa que leia o primeiro termo e a razão de uma Progressão Aritmética.
 No final, mostre os 10 primeiros termos dessa progressão.
 '''
+
+t = int(input('Digite o termo: '))
+r = int(input('Digite a razão: '))
+
+for c in range(t, 1000, r):
+    print(c)
