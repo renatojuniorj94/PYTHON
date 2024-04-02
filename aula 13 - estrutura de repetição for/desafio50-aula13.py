39697ad2fea49768f0b91a40c02246b7397343e2
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-01 23:48:02 -0300

    continuação desafio 50

--- aula 13 - estrutura de repetição for/desafio50-aula13.py
+++ aula 13 - estrutura de repetição for/desafio50-aula13.py
@@ -3,9 +3,8 @@ Desenvolva um programa que leia seis números inteiros e mostre apenas daqueles
 que forem pares. Se o valor digitado for ímpar, desconsidero-o.
 '''
 
-qtd = 0
+
 for c in range(0, 6):
     n = int(input('Digite um número inteiro: '))
-if n % 2 == 0:
-    qtd += 1
-print('Você digitou {} números pares.'.format(n))
+if n == (n % 2 == 0):
+    print()
