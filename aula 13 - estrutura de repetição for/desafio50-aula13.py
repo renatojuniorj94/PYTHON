568b8113b49cd5b971c2a31f75472b2ae5c8e755
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-02 00:22:58 -0300

    resolvendo desafio 50

--- aula 13 - estrutura de repetição for/desafio50-aula13.py
+++ aula 13 - estrutura de repetição for/desafio50-aula13.py
@@ -1,10 +1,13 @@
 '''
-Desenvolva um programa que leia seis números inteiros e mostre apenas daqueles
+Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles
 que forem pares. Se o valor digitado for ímpar, desconsidero-o.
 '''
 
-
-for c in range(0, 6):
-    n = int(input('Digite um número inteiro: '))
-if n == (n % 2 == 0):
-    print()
+soma = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input('Digite o {}º número inteiro: '.format(c)))
+    if n % 2 == 0:
+        soma += n
+        cont += 1
+print('Você informou {} números pares e a soma foi {}'.format(cont, soma))
