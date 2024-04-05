84363a048169bb920add2f6fbd627f7244f53f8c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-04 23:21:32 -0300

    corrigindo desafio 47

--- aula 13 - estrutura de repetição for/desafio47-aula13.py
+++ aula 13 - estrutura de repetição for/desafio47-aula13.py
@@ -4,4 +4,13 @@ que estão no intervalo entre 1 e 50
 '''
 
 for c in range(2, 51, 2):
-    print(c)
+    print(c, end=' ')
+print('\n')
+
+#Correto! :D
+
+#Outra maneira de fazer: (Acima é melhor porque economiza repetições e consequentemente memória)
+
+for i in range(1, 51):
+    if i % 2 == 0:
+        print(i)
