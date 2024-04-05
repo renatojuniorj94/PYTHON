6920f9f3aafb58707e968e993925d933fe907ecb
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-04 23:50:53 -0300

    corrigindo desafio 49

--- aula 13 - estrutura de repetição for/desafio49-aula13.py
+++ aula 13 - estrutura de repetição for/desafio49-aula13.py
@@ -4,7 +4,9 @@ só que agora utilizando um laço for.
 '''
 
 n = int(input('Digite um número inteiro: '))
-print('_' * 12)
+print('_' * 12, '\n')
 for c in range (1, 11):
     print('{} x {} = {}'.format(n, c, n * c))
 print('_' * 12)
+
+#Correto! :D
