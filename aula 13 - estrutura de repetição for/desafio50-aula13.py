21dd484f4ccd480a127017ea82cb4aac4a11527b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-05 01:13:53 -0300

    aprimorando desafio 50

--- aula 13 - estrutura de repetição for/desafio50-aula13.py
+++ aula 13 - estrutura de repetição for/desafio50-aula13.py
@@ -11,3 +11,39 @@ for c in range(1, 7):
         cont += 1
         soma += n
 print('Você informou {} números pares e a soma foi {}'.format(cont, soma))
+
+#Correto! :D
+
+#Outra maneira mais aprimorada de fazer:
+
+soma = 0
+cont = 0
+for c in range(1, 7):
+    n = int(input('Digite o {}º número inteiro: '.format(c)))
+    if n % 2 == 0:
+        cont += 1
+        soma += n
+
+if cont == 0:
+    print('Você não digitou nenhum número par')
+else:
+    print('Você informou {} números pares e a soma foi {}'.format(cont, soma))
+
+#Outra forma de fazer:
+
+soma = 0
+cont = 0
+tem_impar = False
+
+for c in range(1, 7):
+    n = int(input('Digite o {}º número inteiro: '.format(c)))
+    if n % 2 == 0:
+        cont += 1
+        soma += n
+    else:
+        tem_impar = True
+
+if tem_impar:
+    print('Você informou {} números pares e a soma foi {}'.format(cont, soma))
+else:
+    print('Você não digitou nenhum número ímpar')
