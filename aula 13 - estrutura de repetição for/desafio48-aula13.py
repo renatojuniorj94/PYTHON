44ca1a6eaddff3e8c256fa0e22590711c1d59320
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-04 23:45:31 -0300

    corrigindo desafio 48

--- aula 13 - estrutura de repetição for/desafio48-aula13.py
+++ aula 13 - estrutura de repetição for/desafio48-aula13.py
@@ -9,6 +9,21 @@ https://docs.python.org/pt-br/3/library/functions.html#sum
 
 print('A seguir são os número ímpares múltiplos de 3, do 1 ao 500:\n')
 for i in range(3, 501, 6):
-    print(i)
+    print(i, end=' ')
 soma = sum(range(3, 501, 6))
-print('\nO resultado soma foi entre todos os números ímpares, que são múltiplos de 3 foi: {}'.format(soma))
+print('\nO resultado soma foi entre todos os números ímpares, que são múltiplos de 3 foi: {}\n'.format(soma))
+
+#Correto! :D
+
+#Outra maneira de fazer e ainda é melhor:
+
+soma2 = 0
+cont = 0
+for c in range(1, 500, 2):
+    if c % 3 == 0:
+        # Fazendo a contagem de quantos números foram encontrados dentro da repetição.
+        cont = cont + 1 # cont += 1
+        soma2 = soma2 + c # soma2 += c
+        print(c, end=' ')
+print('\n')
+print('A soma de todos os {} valores múltiplos de 3 foi: {}'.format(cont, soma2, end=' '))
