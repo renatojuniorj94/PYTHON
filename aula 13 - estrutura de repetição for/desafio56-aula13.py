bd16c1ad9719827d1aff824db6bc73b4ae8f049b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-10 00:57:16 -0300

    desafio 56

--- aula 13 - estrutura de repetição for/desafio56-aula13.py
+++ aula 13 - estrutura de repetição for/desafio56-aula13.py
@@ -5,3 +5,13 @@ do programa, mostre:
 - Qual é o nome do homem mais velho.
 - Quantas mulheres tem menos de 20 anos.
 '''
+
+for c in range(1, 5):
+    print('----- {}ª pessoa -----'.format(c))
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).lower()
+
+print('A média de idade do grupo é de {} anos.')
+print('O homem mais velho tem {} anos e se chama {}.')
+print('Ao todo são {} mulheres com menos de 20 anos.')
