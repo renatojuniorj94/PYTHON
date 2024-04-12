f6a9483d997abb5cb00b519c8d991d139be563d3
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-12 01:00:24 -0300

    desafio 56
    Resolução desafio 56

--- aula 13 - estrutura de repetição for/desafio56-aula13.py
+++ aula 13 - estrutura de repetição for/desafio56-aula13.py
@@ -6,12 +6,33 @@ do programa, mostre:
 - Quantas mulheres tem menos de 20 anos.
 '''
 
+soma_idade = 0
+media_idade = 0
+maior_idade_homem = 0
+homem_velho = 0
+mulher_20 = 0
+
 for c in range(1, 5):
     print('----- {}ª pessoa -----'.format(c))
     nome = str(input('Nome: ')).strip()
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo [M/F]: ')).lower()
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    soma_idade += idade
 
-print('A média de idade do grupo é de {} anos.'.format(idade / c))
-print('O homem mais velho tem {} anos e se chama {}.')
-print('Ao todo são {} mulheres com menos de 20 anos.')
+    if sexo not in ['M', 'm', 'f', 'F']:
+        print('\033[31mSexo inválido!\033[m')
+        exit()
+
+    if c == 1 and sexo in 'Mm':
+        maior_idade_homem = idade
+        homem_velho = nome
+    if sexo in 'Mm' and idade > maior_idade_homem:
+        maior_idade_homem = idade
+        homem_velho = nome
+    if sexo in 'Ff' and idade < 20:
+        mulher_20 += 1
+media_idade = soma_idade / 4
+
+print('A média de idade do grupo é de {:.0f} anos.'.format(media_idade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maior_idade_homem, homem_velho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(mulher_20))
