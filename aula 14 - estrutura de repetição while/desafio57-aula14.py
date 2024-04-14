9f4babe9692a6701aa08c3a504c63191171ef65c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-14 00:52:15 -0300

    desafio 57

--- aula 14 - estrutura de repetição while/desafio57-aula14.py
+++ aula 14 - estrutura de repetição while/desafio57-aula14.py
@@ -2,3 +2,17 @@
 Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' e 'F'.
 Caso esteja errado, peça a digitação novamente até ter um valor correto.
 '''
+
+'''s = 'M', 'm', 'F', 'f'
+teste = str(input('Digite o sexo [M/F]: '))
+while teste == s:
+    print(teste)
+'''
+
+#Feito com IA
+while True:
+    sexo = input("Digite o sexo (M/F): ").upper()
+    if sexo == 'M' or sexo == 'F':
+        print(sexo)
+    else:
+        print("Sexo inválido. Por favor, digite 'M' para masculino ou 'F' para feminino.")
