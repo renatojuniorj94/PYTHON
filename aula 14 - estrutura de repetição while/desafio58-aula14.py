e7718dd33be202d8b728e70acf321e702cc25751
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-15 01:59:30 -0300

    desafio 59
    Atualizei alguns comandos do desafio 58

--- aula 14 - estrutura de repetição while/desafio58-aula14.py
+++ aula 14 - estrutura de repetição while/desafio58-aula14.py
@@ -14,15 +14,15 @@ jogador = int(input('Em que número eu pensei? '))
 computador = randint(0, 10)
 contador = 0
 print('PROCESSANDO...')
-sleep(3)
+sleep(1)
 
 while computador != jogador:
-    print('Você errou! Eu digitei {} e você digitou {}.'.format(computador, jogador))
+    print('GANHEI! Eu digitei {} e você digitou {}.'.format(computador, jogador))
     computador = randint(0, 10)
     jogador = int(input('Em que número eu pensei? (de 0 a 10) '))
     print('PROCESSANDO...')
     sleep(1)
     contador += 1
     if jogador == computador:
-        print('Parabéns! Você ganhou! Eu digitei {} e você digitou {}'.format(computador, jogador))
+        print('Você ganhou, PARABÉNS! Eu digitei {} e você digitou {}.'.format(computador, jogador))
 print('Você precisou de {} tentativas para acertar.'.format(contador + 1))
--- aula 14 - estrutura de repetição while/desafio59-aula14.py
+++ aula 14 - estrutura de repetição while/desafio59-aula14.py
@@ -8,3 +8,60 @@ Crie um programa que leia dois valores e mostre um menu na tela:
 
 Seu programa deverá realizar a operação solicitada em cada caso.
 '''
+
+v1 = float(input('Digite o primeiro valor: '))
+v2 = float(input('Digite o segundo valor: '))
+o = int(input('Você deseja:\n'
+    '[1] Somar\n'
+    '[2] Multiplicar\n'
+    '[3] Maior\n'
+    '[4] Novos números\n'
+    '[5] Sair do programa\n>>> '))
+
+while o != 5:
+    if o == 1:
+        print('{:.0f}'.format(v1 + v2))
+        v1 = float(input('Digite o primeiro valor: '))
+        v2 = float(input('Digite o segundo valor: '))
+        o = int(input('Você deseja:\n'
+                      '[1] Somar\n'
+                      '[2] Multiplicar\n'
+                      '[3] Maior\n'
+                      '[4] Novos números\n'
+                      '[5] Sair do programa\n>>> '))
+    if o == 2:
+        print('{:.0f}'.format(v1 * v2))
+        v1 = float(input('Digite o primeiro valor: '))
+        v2 = float(input('Digite o segundo valor: '))
+        o = int(input('Você deseja:\n'
+                      '[1] Somar\n'
+                      '[2] Multiplicar\n'
+                      '[3] Maior\n'
+                      '[4] Novos números\n'
+                      '[5] Sair do programa\n>>> '))
+    if o == 3:
+        if v1 > v2:
+            print(v1, " é o maior.")
+        elif v1 < v2:
+            print(v2, ' é o maior')
+        else:
+            print('Os números são iguais')
+        v1 = float(input('Digite o primeiro valor: '))
+        v2 = float(input('Digite o segundo valor: '))
+        o = int(input('Você deseja:\n'
+                      '[1] Somar\n'
+                      '[2] Multiplicar\n'
+                      '[3] Maior\n'
+                      '[4] Novos números\n'
+                      '[5] Sair do programa\n>>> '))
+    if o == 4:
+        v1 = float(input('Digite o primeiro valor: '))
+        v2 = float(input('Digite o segundo valor: '))
+        o = int(input('Você deseja:\n'
+                      '[1] Somar\n'
+                      '[2] Multiplicar\n'
+                      '[3] Maior\n'
+                      '[4] Novos números\n'
+                      '[5] Sair do programa\n>>> '))
+print('Obrigado por usar nosso sistema, até breve!')
+exit()
