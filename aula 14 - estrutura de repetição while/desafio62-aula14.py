0174f911d5375f9b22b1bc74dc041ce1e242090c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-16 01:03:33 -0300

    desafio 62

--- aula 14 - estrutura de repetição while/desafio62-aula14.py
+++ aula 14 - estrutura de repetição while/desafio62-aula14.py
@@ -2,3 +2,19 @@
 Melhore o desafio 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerra
 quando ele disser que quer mostrar 0 termos.
 '''
+
+termo = int(input('Digite o termo: '))
+razão = int(input('Digite a razão: '))
+pa = termo
+cont = 1
+mais = 10
+total = 0
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} > '.format(pa), end='')
+        pa += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Você quer exibir mais quantos termos? '))
+print('Progressão finalizada com {} termos mostrados'.format(total))
