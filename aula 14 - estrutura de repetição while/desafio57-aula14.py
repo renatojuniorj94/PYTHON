6c5fb8d7bafde0625299db14435a11037c4186de
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-19 21:11:08 -0300

    desafio 57
    resolução desafio 57

--- aula 14 - estrutura de repetição while/desafio57-aula14.py
+++ aula 14 - estrutura de repetição while/desafio57-aula14.py
@@ -4,9 +4,19 @@ Caso esteja errado, peça a digitação novamente até ter um valor correto.
 '''
 
 #Feito com IA
-while True:
-    sexo = input("Digite o sexo (M/F): ").upper()
+'''while True:
+    #[0] Pega somente a primeira letra
+    sexo = input("Digite o sexo (M/F): ").upper().strip()[0]
     if sexo == 'M' or sexo == 'F':
-        print(sexo)
+        print('Sexo {} registrado com sucesso!'.format(sexo))
+        exit()
     else:
-        print("Sexo inválido. Por favor, digite 'M' para masculino ou 'F' para feminino.")
+        print("Sexo inválido. Por favor, digite 'M' para masculino ou 'F' para feminino.")'''
+
+#Correto! :D
+
+#Outra maneira de fazer
+sexo2 = input("Digite de novo o sexo (M/F): ").upper().strip()[0]
+while sexo2 not in 'MmFf':
+    sexo2 = input('Dados inválidos. Por favor, informe seu sexo: ').strip().upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo2))
