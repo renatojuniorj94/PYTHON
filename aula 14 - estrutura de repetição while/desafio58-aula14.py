44c0024cf2cccfdc0640f2e4a9606dd1f8b7d5ab
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-20 01:09:24 -0300

    desafio 58
    resolução do desafio 58

--- aula 14 - estrutura de repetição while/desafio58-aula14.py
+++ aula 14 - estrutura de repetição while/desafio58-aula14.py
@@ -7,7 +7,7 @@ necessários para vencer.
 from random import randint
 from time import sleep
 
-print('-=-' * 20)
+'''print('-=-' * 20)
 print('Vou pensar em número entre 0 e 10. Tente adivinhar...')
 print('-=-' * 20)
 jogador = int(input('Em que número eu pensei? '))
@@ -25,4 +25,25 @@ while computador != jogador:
     contador += 1
     if jogador == computador:
         print('Você ganhou, PARABÉNS! Eu digitei {} e você digitou {}.'.format(computador, jogador))
-print('Você precisou de {} tentativas para acertar.'.format(contador + 1))
+print('Você precisou de {} tentativas para acertar.'.format(contador + 1))'''
+
+#Correto! :D
+
+#Outra maneira de fazer
+
+computador = randint(0, 10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpite = 0
+while not acertou:
+    jogador = int(input('Qual o seu palpite? '))
+    palpite += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais... Tente mais uma vez.')
+        elif jogador > computador:
+            print('Menos... Tente mais uma vez.')
+print('Acertou com {} tentativas. Parabéns!'.format(palpite))
