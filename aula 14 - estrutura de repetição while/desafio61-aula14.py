46d13eeaaba032bb51f599a2db99232e98575301
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-22 16:30:02 -0300

    resolução desafio 61

--- aula 14 - estrutura de repetição while/desafio61-aula14.py
+++ aula 14 - estrutura de repetição while/desafio61-aula14.py
@@ -2,7 +2,9 @@
 Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA,
 mostrando o 10 primeiros termos da progressão usando a estrutura while.
 '''
-
+print('=' * 30)
+print('{:^30}'.format('10 TERMOS DE UMA PA'))
+print('=' * 30)
 termo = int(input('Digite o termo: '))
 razão = int(input('Digite a razão: '))
 pa = termo
@@ -13,3 +15,5 @@ while cont <= 10:
     pa += razão
     cont += 1
 print('FIM')
+
+#Correto! :D
