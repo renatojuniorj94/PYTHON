f8e7ee893a97857c98efee7011da0b237782ddb9
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-22 03:18:23 -0300

    desafio 60
    resolução do desafio

--- aula 14 - estrutura de repetição while/desafio60-aula14.py
+++ aula 14 - estrutura de repetição while/desafio60-aula14.py
@@ -4,9 +4,23 @@ Ex:
 5! = 5 x 4 x 3 x 2 x 1 = 120
 (Fazer esse desafio com while e com for)
 '''
-n = int(input('Digite um número: '))
+'''n = int(input('Digite um número: '))
 c = n
+fat = 1
+print('Calculando {}! = '.format(n), end='')
 while c > 0:
     print('{}'.format(c), end='')
     print(' x ' if c > 1 else ' = ', end='')
+    fat *= c
     c -= 1
+print('{}'.format(fat))'''
+
+#Usando for
+from math import factorial
+n = int(input('Digite um número: '))
+fat = n
+for c in range(n, 0, -1):
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    c *= n
+print(factorial(n))
