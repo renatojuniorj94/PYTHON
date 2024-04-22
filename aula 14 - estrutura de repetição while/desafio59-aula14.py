36f92243a936c0c8480eba30c0289471ffc844c6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-22 00:52:50 -0300

    desafio 59
    resolução do desafio

--- aula 14 - estrutura de repetição while/desafio59-aula14.py
+++ aula 14 - estrutura de repetição while/desafio59-aula14.py
@@ -9,7 +9,7 @@ Crie um programa que leia dois valores e mostre um menu na tela:
 Seu programa deverá realizar a operação solicitada em cada caso.
 '''
 
-v1 = float(input('Digite o primeiro valor: '))
+'''v1 = float(input('Digite o primeiro valor: '))
 v2 = float(input('Digite o segundo valor: '))
 o = int(input('Você deseja:\n'
     '[1] Somar\n'
@@ -64,7 +64,7 @@ while o != 5:
                       '[4] Novos números\n'
                       '[5] Sair do programa\n>>> '))
 print('Obrigado por usar nosso sistema, até breve!')
-exit()
+exit()'''
 
 #Correto! :D
 
@@ -72,3 +72,42 @@ exit()
 
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
+opção = 0
+
+while opção != 5:
+    print('''[1] SOMAR
+[2] MULTIPLICAR
+[3] MAIOR
+[4] NOVOS NÚMERO
+[5] SAIR DO PROGRAMA''')
+    opção = int(input('Digite sua opção >>> '))
+    if opção == 1:
+        print('\033[1;31m{}\033[m'.format(n1 + n2))
+        print('=*' * 10, end='')
+        print('=')
+    elif opção == 2:
+        print('\033[1;31m{}\033[m'.format(n1 * n2))
+        print('=*' * 10, end='')
+        print('=')
+    elif opção == 3:
+        if n1 > n2:
+            print('\033[1;31m{}\033[m'.format(n1))
+            print('=*' * 10, end='')
+            print('=')
+        else:
+            print('\033[1;31m{}\033[m'.format(n2))
+            print('=*' * 10, end='')
+            print('=')
+    elif opção == 4:
+        n1 = int(input('Digite o primeiro valor: '))
+        n2 = int(input('Digite o segundo valor: '))
+        print('=*' * 10, end='')
+        print('=')
+    elif opção == 5:
+        print('\033[1;32mObrigado por usar nosso sistema!\033[m')
+    else:
+        print('\033[31mOpção inválida!\033[m Tente novamente...')
+        n1 = int(input('Digite o primeiro valor: '))
+        n2 = int(input('Digite o segundo valor: '))
+        print('=*' * 10, end='')
+        print('=')
