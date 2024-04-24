d9ceac763d919b5ab0b919d673233cc8c9671e70
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-24 02:08:29 -0300

    desafio 63
    resolução desafio 63

--- aula 14 - estrutura de repetição while/desafio63-aula14.py
+++ aula 14 - estrutura de repetição while/desafio63-aula14.py
@@ -5,4 +5,20 @@ Ex:
 0 > 1 > 1 > 2 > 3 > 5 > 8
 '''
 
-n = int(input('Digite um número: '))
+print('=-' * 16 + '=')
+print('{:^31}'.format('Projeção de fibonacci'))
+print('=-' * 16 + '=')
+n = int(input('Quantos termos você quer exibir? '))
+t1= 0
+t2 = 1
+cont = 3
+print('{} > {}'.format(t1, t2), end='')
+while cont <= n:
+    t3 = t1 + t2
+    print(' > {}'.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' > FIM')
+
+#Correto! :D
