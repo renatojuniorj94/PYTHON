82b0acaed8998ffbdb12e707f790ebbd67cf838f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-25 18:10:49 -0300

    desafio 64
    resolução desafio 64

--- aula 14 - estrutura de repetição while/desafio64-aula14.py
+++ aula 14 - estrutura de repetição while/desafio64-aula14.py
@@ -5,7 +5,7 @@ No final mostre quantos números foram digitados e qual foi a soma entre eles
 (desconsiderando o flag)
 '''
 
-num = int(input('Digite um número(999 para parar): '))
+'''num = int(input('Digite um número(999 para parar): '))
 contador = 0
 soma = 0
 while num != 999:
@@ -15,5 +15,21 @@ while num != 999:
     soma += num
     #jogamos a variável 'num' por último para não incluir 999 na 'variável' soma
     num = int(input('Digite um número(999 para parar): '))
-print('Você digitou {} números e a soma entre eles foi {}'.format(contador, soma))
+print('Você digitou {} números e a soma entre eles foi {}'.format(contador, soma))'''
+
+#Correto! :D
+#Outra maneira de fazer
+
+'''num = 0
+cont = 0
+soma = 0'''
+#Simplificando as variáveis acima.
+num = cont = soma = 0
+while num != 999:
+    num = int(input('Digite um número(999 para parar): '))
+    soma += num
+    cont += 1
+#Abaixo é uma gambiarra
+#Para corrigir, colocamos a variável 'num' como input antes da repetição while, e dentro de while, na última linha
+print('Você digitou {} números e a soma entre eles foi {}.'.format(cont - 1, soma - 999))
 
