2c68cf2fb4e74492448fc10076e4921dc828d76b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-26 02:50:56 -0300

    desafio 65
    resolução desafio 65

--- aula 14 - estrutura de repetição while/desafio65-aula14.py
+++ aula 14 - estrutura de repetição while/desafio65-aula14.py
@@ -4,3 +4,41 @@ da execução, mostre a média entre todos os valores e qual foi o maior e o
 menor valores lidos. O programa deve perguntar ao usuário se ele quer ou
 não continuar a digitar valores.
 '''
+num = int(input('Digite um número inteiro: '))
+cond = input('Deseja continuar? [S/N] ').upper()
+soma = num
+contador = 1
+maior = menor = num
+
+while cond == 'S':
+    contador += 1
+    num = int(input('Digite um número inteiro: '))
+    if num > maior:
+        maior = num
+    elif num < menor:
+        menor = num
+    cond = input('Deseja continuar? [S/N] ').upper()
+    soma += num
+print('Você digitou {} número e a média entre eles foi {}'.format(contador, soma / contador))
+print('O maior número foi {} e o menor {}'.format(maior, menor))
+
+#Correto! :D
+#Outra maneira de fazer
+
+resp = 'S'
+soma = quant = media = maior = menor = 0
+while resp in 'Ss':
+    num = int(input('Digite um número: '))
+    soma += num
+    quant += 1
+    if quant == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    resp = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+media = soma / quant
+print('Você digitou {} números e a média foi {}'.format(quant, media))
+print('O maior número foi {} e o menor {}'.format(maior, menor))
