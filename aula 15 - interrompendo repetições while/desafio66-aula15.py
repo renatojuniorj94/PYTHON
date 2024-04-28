d59754a46ba8268d8c4fa2f812bf66f6e74e5c50
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-28 17:17:34 -0300

    desafio 66

--- aula 15 - interrompendo repetições while/desafio66-aula15.py
+++ aula 15 - interrompendo repetições while/desafio66-aula15.py
@@ -3,3 +3,16 @@ Crie um programa que leia vários números inteiros pelo teclado. O programa só
 digitar o valor 999, que é a condição de parada. No final, mostre quantos números foram digitados e qual
 foi a soma entre eles (desconsiderando o flag)
 '''
+
+'''num = int(input('Digite um número (999 para parar): '))
+'''
+contador = soma = 0
+while True:
+    num = int(input('Digite um número (999 para parar): '))
+    if num == 999:
+        break
+    contador += 1
+    soma += num
+    if num == 999:
+        break
+print(f'Você digitou {contador} números e a soma entre eles foi {soma}')
