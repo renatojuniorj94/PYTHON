008e178149a49d6f0129dff119eabe3d5692f7cd
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-04-29 23:42:54 -0300

    desafio 67
    Primeira tentativa

--- aula 15 - interrompendo repetições while/desafio67-aula15.py
+++ aula 15 - interrompendo repetições while/desafio67-aula15.py
@@ -2,3 +2,15 @@
 Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado
 pelo usuário. O programa será interrompido quando o número solicitado for negativo.
 '''
+print('=' * 30)
+print(f'{'Desafio Tabuada':^30}')
+print('=' * 30)
+
+num = int(input('Digite um número para a tabuada: '))
+tab = 1
+while True:
+    print(f'{num} x {tab} = {num * tab}')
+    tab += 1
+    if tab > 10:
+        break
+
