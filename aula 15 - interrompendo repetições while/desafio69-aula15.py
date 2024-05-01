faecd3bde46a2c58b1cebb9dd3a38ed7da770fca
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-01 20:13:46 -0300

    desafio 69

--- aula 15 - interrompendo repetições while/desafio69-aula15.py
+++ aula 15 - interrompendo repetições while/desafio69-aula15.py
@@ -5,3 +5,37 @@ A - Quantas pessoas tem mais de 18 anos.
 B - Quantos homens foram cadastrados.
 C - Quantas mulheres tem menos de 20 anos.
 '''
+print('=' * 30)
+print(f'{'CADASTRO DE USUÁRIOS':^30}')
+print('=' * 30)
+maior_idade = homens = mulher_menor_idade = 0
+while True:
+    idade = int(input('Idade: '))
+    if idade >= 18:
+        maior_idade += 1
+    while idade < 0:
+        print('Opção \033[1;43mINVÁLIDA!\033[m')
+        idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
+    if sexo == 'M':
+        homens += 1
+    if sexo == 'F' and idade < 20:
+        mulher_menor_idade += 1
+    while sexo not in ['M', 'F']:
+        print('Opção \033[1;43mINVÁLIDA!\033[m')
+        sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
+    continuação = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
+    usuários += 1
+    if continuação == 'N':
+        print('—' * 30)
+        break
+    while continuação not in ['S', 'N']:
+        print('Opção \033[1;43mINVÁLIDA!\033[m')
+        continuação = str(input('Quer continuar [S/N]: ')).upper().strip()[0]
+    print('—' * 30)
+print('=====FIM DO PROGRAMA=====')
+print(f'Total de pessoas com mais de 18 anos: {maior_idade}')
+print(f'Ao total temos {homens} homens cadastrados.')
+print(f'E temos {mulher_menor_idade} mulheres com menos de 20 anos.')
+
+#Correto! :D
