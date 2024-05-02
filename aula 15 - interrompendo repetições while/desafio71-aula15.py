f07e0f0ba4f5f91595869dbfe5a5808c01df63fe
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-02 02:51:00 -0300

    desafio 71

--- aula 15 - interrompendo repetições while/desafio71-aula15.py
+++ aula 15 - interrompendo repetições while/desafio71-aula15.py
@@ -3,3 +3,26 @@ Crie um programa que simule o funcionamento de um caixa eletrônico. No início,
 valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues.
 OBS: considere que o caixa possui cédulas de R$ 50, R$ 20, R$ 10 e R$1
 '''
+valor = int(input('Qual valor você deseja sacar? '))
+valor_total = valor
+nota = 50
+total_nota = 0
+while True:
+    if valor_total >= nota:
+        valor_total -= nota
+        total_nota += 1
+    else:
+        if total_nota > 0:
+            print(f'Total de {total_nota} cédulas de R$ {nota}')
+        if nota == 50:
+            nota = 20
+        elif nota == 20:
+            nota = 10
+        elif nota == 10:
+            nota = 1
+        total_nota = 0
+        if valor_total == 0:
+            break
+print('fim')
+
+#Correto! :D
