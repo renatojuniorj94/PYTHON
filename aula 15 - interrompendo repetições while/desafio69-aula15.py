ccfba974bfbfd5b0ff3860079e9d43308b8f7563
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-04 01:46:05 -0300

    Resolução desafio 69

--- aula 15 - interrompendo repetições while/desafio69-aula15.py
+++ aula 15 - interrompendo repetições while/desafio69-aula15.py
@@ -8,7 +8,7 @@ C - Quantas mulheres tem menos de 20 anos.
 print('=' * 30)
 print(f'{'CADASTRO DE USUÁRIOS':^30}')
 print('=' * 30)
-maior_idade = homens = mulher_menor_idade = 0
+maior_idade = homens = mulher_menor_idade = usuários = 0
 while True:
     idade = int(input('Idade: '))
     if idade >= 18:
@@ -39,3 +39,25 @@ print(f'Ao total temos {homens} homens cadastrados.')
 print(f'E temos {mulher_menor_idade} mulheres com menos de 20 anos.')
 
 #Correto! :D
+#Outra maneira de fazer
+
+'''tot18 = totH = totM20 = 0
+while True:
+    idade = int(input('Idade: '))
+    sexo = ' '
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).strip().upper()[0]
+    if idade >= 18:
+        tot18 += 1
+    if sexo == 'M':
+        totH += 1
+    if sexo == 'F' and idade < 20:
+        totM20 += 1
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print(f'Total de pessoas com mais de 18 anos: {tot18}')
+print(f'Ao todo temos {totH} homens cadastrados')
+print(f'E temos {totM20} mulheres com menos de 20 anos.')'''
