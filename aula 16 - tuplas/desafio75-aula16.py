7db08dfeb5ebf3a60b66125c162da21fae88e515
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-06 01:43:29 -0300

    Enunciado dos desafios 74 ao 77

---
+++ aula 16 - tuplas/desafio75-aula16.py
@@ -0,0 +1,7 @@
+'''
+Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla.
+No final, mostre:
+A - Quantos vezes apareceu o valor 9.
+B - Em que posição foi digitado o primeiro valor 3.
+C - Quais foram os números pares.
+'''
