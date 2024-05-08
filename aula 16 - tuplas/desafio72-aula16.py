d576269ce6a59e41d304f738fb1d09dd6d23c074
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-08 03:52:11 -0300

    resolução desafio 72

--- aula 16 - tuplas/desafio72-aula16.py
+++ aula 16 - tuplas/desafio72-aula16.py
@@ -3,7 +3,7 @@ Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por 
 de zero até vinte.
 Seu programa deverá ler um número pelo teclado (entre 0 e 20) e amostrá-lo por extenso.
 '''
-tupla = ('zero', 'hum', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+'''tupla = ('zero', 'hum', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
          'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 num = int(input('Digite um número entre 0 e 20: '))
 while num < 0 or num > 20:
@@ -11,6 +11,16 @@ while num < 0 or num > 20:
     num = int(input('Digite um número entre 0 e 20: '))
 for pos in enumerate(tupla):
     print(f'Você digitou o número {tupla[num]}')
-    break
+    break'''
 
 #Correto! :D
+#Outra maneira de fazer:
+
+cont = ('zero', 'hum', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+        'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    núm = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= núm <= 20:
+        break
+    print('Tente novamente.', end=' ')
+print(f'Você digitou o número {cont[núm]}')
