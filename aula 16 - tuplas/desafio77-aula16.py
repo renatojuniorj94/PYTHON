4784e6a69e39d0aea53bfe51f85acd753bea637b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-08 01:54:05 -0300

    desafio 77

--- aula 16 - tuplas/desafio77-aula16.py
+++ aula 16 - tuplas/desafio77-aula16.py
@@ -2,3 +2,14 @@
 Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 '''
+
+tupla = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar',
+         'trabalhar', 'mercado', 'programador', 'futuro')
+
+for palavra in tupla:
+    print(f'\nNa palavra {palavra.upper()} temos', end=' ')
+    for letra in palavra:
+        if letra in 'aeiou':
+            print(letra, end=' ')
+
+#Correto! :D
