47f004f9876792e8be3d3bf5ed49623e6fdf5674
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-09 19:38:06 -0300

    resolução desafio 76

--- aula 16 - tuplas/desafio76-aula16.py
+++ aula 16 - tuplas/desafio76-aula16.py
@@ -1,20 +1,18 @@
-'''
-Crie um programa que tenha uma tupla única com nomes de produtos e seus
+""" Crie um programa que tenha uma tupla única com nomes de produtos e seus
 respectivos preços, na sequência.
-No final, mostre uma listagem de preços, organizando os dados em forma tabular.
-'''
+No final, mostre uma listagem de preços, organizando os dados em forma tabular. """
 
 listagem = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.90, 'Estojo', 25, 'Transferidor', 4.20,
             'Compass', 9.99, 'Mochila', 120.32, 'Canetas', 22.30, 'Livro', 34.90)
-print('—' * 3)
-print(f'{'LISTAGEM D PREÇOS':^30}')
-print('—' * 3)
-print(f'{listagem[0]}.............R$ {listagem[1]}')
-print(f'{listagem[2]}.............R$ {listagem[3]}')
-print(f'{listagem[4]}.............R$ {listagem[5]}')
-print(f'{listagem[6]}.............R$ {listagem[7]}')
-print(f'{listagem[8]}.............R$ {listagem[9]}')
-print(f'{listagem[10]}............R$ {listagem[11]}')
-print(f'{listagem[12]}............R$ {listagem[13]}')
-print(f'{listagem[14]}............R$ {listagem[15]}')
-print(f'{listagem[16]}............R$ {listagem[17]}')
+print('—' * 30)
+print(f'{'LISTAGEM DE PREÇOS':^30}')
+print('—' * 30)
+
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('—' * 40)
+
+#Correto! :D
