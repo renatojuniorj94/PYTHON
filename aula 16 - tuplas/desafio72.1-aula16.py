669ec16944dfff48375019740e9a9e23d81877b0
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-11 23:59:51 -0300

    parte 2

--- aula 16 - tuplas/desafio72.1-aula16.py
+++ aula 16 - tuplas/desafio72.1-aula16.py
@@ -3,16 +3,12 @@ cont = ('zero', 'hum', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito
 
 num = int(input('Digite um número entre 0 e 20: '))
 while True:
-    if num < 0 or num > 20:
+    while num < 0 or num > 20:
         print('Número inválido!')
         num = int(input('Digite um número entre 0 e 20: '))
     if num >= 0 or num <= 20:
         print(f'Você digitou o número {cont[num]}')
         continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
-        if continuar == 'S':
-            num = int(input('Digite um número entre 0 e 20: '))
-        elif continuar != 'SN':
-            print('Opção inválida!')
-            continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
-        if continuar == 'N':
-            break
+    while continuar == 'S':
+        num = int(input('Digite um número entre 0 e 20: '))
+
