597ce2837cb295f5ed0bd3b5e2c711724b77719c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-13 23:45:47 -0300

    desafio 78 (parte 1)

--- aula 17 - listas/desafio78-aula17.py
+++ aula 17 - listas/desafio78-aula17.py
@@ -3,3 +3,8 @@ Faça um programa que leia 5 valores numéricos e guarde-os em uma lista.
 No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas
 posições na tabela
 """
+num = []
+for c in range(0, 5):
+    num.append(int(input('Digite um número: ')))
+
+print(num)
