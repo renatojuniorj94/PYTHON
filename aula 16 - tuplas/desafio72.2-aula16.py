0ce89b9fb8cb12d030c101a523f0e0c2e5311a78
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-16 00:12:17 -0300

    versão final desafio 72.2

--- aula 16 - tuplas/desafio72.2-aula16.py
+++ aula 16 - tuplas/desafio72.2-aula16.py
@@ -1,26 +1,18 @@
 cont = ('zero', 'hum', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
         'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 
-num = int(input('Digite um número entre 0 e 20: '))
 while True:
+    num = int(input('Digite um número entre 0 e 20: '))
     while num < 0 or num > 20:
         print('Número inválido!')
         num = int(input('Digite um número entre 0 e 20: '))
-    print(f'Você digitou o número {cont[num]}')
-    if num >= 0 or num <= 20:
-        continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
-    if continuar == 'S':
-        num = int(input('Digite um número entre 0 e 20: '))
-        continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
-    elif continuar != 'S' or continuar != 'N':
-        print('Opção inválida!')
-    else:
-        break
 
-''' while num < 0 or num > 20:
-print('Número inválido!')
-num = int(input('Digite um número entre 0 e 20: '))
-print(f'Você digitou o número {cont[num]}')
-continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
-if continuar == 'N':
-break'''
+    print(f'Você digitou o número {cont[num]}')
+
+    continuar = input('Deseja continuar? [S/N] ').strip().upper()
+    while continuar not in ('S', 'N'):
+        print('Opção inválida!')
+        continuar = input('Deseja continuar? [S/N] ').strip().upper()
+
+    if continuar == 'N':
+        break
