f070365ad00925b2828e3cc24c1ba760546fa96e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-17 03:03:03 -0300

    desafio 79
    Estrutura do projeto

--- aula 17 - listas/desafio79-aula17.py
+++ aula 17 - listas/desafio79-aula17.py
@@ -3,3 +3,14 @@ Crie um programa onde o usuário possa digitar vários valores numéricos
 e cadastre-os em uma lista. Caso o número já exista lá dentro, ele não será adicionado.
 No final, serão exibidos todos os valores únicos digitados, em ordem crescente.
 """
+
+num = [int(input('Digite um número: '))]
+print('Valor adicionado com sucesso...')
+while True:
+    continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if continuar == 'S':
+        num = [int(input('Digite um número: '))]
+    elif continuar not in 'SN':
+        print('Opção inválida!')
+    else:
+        break
