0a95b64ca8f157886ea466022c7c3e24782d7cb6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-20 22:28:27 -0300

    desafio 81
    resolução desafio 81

--- aula 17 - listas/desafio81-aula17.py
+++ aula 17 - listas/desafio81-aula17.py
@@ -5,3 +5,22 @@ A - Quantos números foram digitados.
 B - A lista de valores, ordenada de forma decrescente.
 C - Se o valor 5 foi digitado e está ou não na lista
 """
+
+valores = list()
+contador = 0
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    contador += 1
+    continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    while continuar not in 'SN':
+        print('Opção inválida!')
+        continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if continuar == 'N':
+        break
+if 5 in valores:
+    print('O valor 5 está na lista')
+else:
+    print('O número 5 não está na lista')
+print(f'Foram digitados {contador} números.')
+valores.sort(reverse=True)
+print(valores)
