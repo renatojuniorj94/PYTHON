a46187137fe3ddaffcabedf5095b3bc977aa2462
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-24 23:45:53 -0300

    desafio 83

--- aula 17 - listas/desafio83-aula17.py
+++ aula 17 - listas/desafio83-aula17.py
@@ -3,3 +3,5 @@ Crie um programa onde o usuário digite uma expressão qualquer
 que use parênteses. Seu aplicativo deverá analisar se a expressão
 passada está com os parênteses abertos e fechados na ordem correta.
 """
+
+expressão = input('Digite uma expressão: ')
