6b4293e50a44fd9c0b1280039dc7bf06373ac293
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-05-27 23:57:11 -0300

    Correção desafio 78

--- aula 17 - listas/desafio78-aula17.py
+++ aula 17 - listas/desafio78-aula17.py
@@ -10,3 +10,29 @@ print(f'O maior valor digitado foi {max(num)} e menor valor digitado foi {min(nu
 for c, v in enumerate(num):
     print(f'Na posição {c} encontrei o valor {v}!')
 
+#Correto! :D
+#Outra maneira de fazer...
+
+listanum = []
+mai = men = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c}: ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+print('=-' * 30)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado foi {mai} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == mai:
+        print(f'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {men} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}... ', end='')
+print()
