0dd65f292edc149639443005e8466b61fcf5ec36
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-02 00:38:18 -0300

    aula 18 - parte 1

--- aula 18 - lista (parte 2)/script-aula18.py
+++ aula 18 - lista (parte 2)/script-aula18.py
@@ -6,7 +6,16 @@ print(pessoas[1])
 print(pessoas[0][0])
 #Pedro
 
+for p in pessoas:
+    print(p[0])
+#Imprindo somente os nomes
+
 teste = list()
 teste.append('Renato')
 teste.append(30)
-galera = 
+galera = list()
+galera.append(teste[:]) # Criando cópia
+teste[0] = 'Maria'
+teste[1] = 22
+galera.append(teste[:])
+print(galera)
