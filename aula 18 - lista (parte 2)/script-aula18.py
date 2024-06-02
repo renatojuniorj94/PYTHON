3ca1ca8ae9d809a61b7a04dc7c6a60d1adf6f0c7
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-01 23:59:38 -0300

    aula 18

---
+++ aula 18 - lista (parte 2)/script-aula18.py
@@ -0,0 +1,12 @@
+pessoas = [['Pedro', 25], ['Maria', 19], ['João', 32]]
+
+print(pessoas[1])
+#['Maria', 19]
+
+print(pessoas[0][0])
+#Pedro
+
+teste = list()
+teste.append('Renato')
+teste.append(30)
+galera = 
