0362a975f3bdeeea537131d92c42c3ab9f75e97f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-03 23:50:40 -0300

    Iniciando desafio 84
    Cadastro pessoas feito com laço de repetição while - ok
    Quantas pessoas foram cadastradas - ok

--- aula 18 - lista (parte 2)/desafio84-aula18.py
+++ aula 18 - lista (parte 2)/desafio84-aula18.py
@@ -5,3 +5,20 @@ A - Quantas pessoas foram cadastradas.
 B - Uma listagem com as pessoas mais pesadas.
 C - Uma listagem com as pessoas mais leves.
 '''
+
+pessoas = []
+totPessoas = 0
+while True:
+    nome = str(input('Nome: '))
+    pessoas.append(nome)
+    peso = float(input('Peso: '))
+    pessoas.append(peso)
+    totPessoas += 1
+    continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
+    if continuar == 'N':
+        break
+    while continuar not in 'SN':
+        print('Opção inválida!')
+        continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
+
+print(pessoas)
