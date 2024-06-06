cab9191a39b2bccf384fdc6a4ceff8a5fd00289d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-05 23:34:15 -0300

    Versão final desafio 84

--- aula 18 - lista (parte 2)/desafio84-aula18.py
+++ aula 18 - lista (parte 2)/desafio84-aula18.py
@@ -6,15 +6,25 @@ B - Uma listagem com as pessoas mais pesadas.
 C - Uma listagem com as pessoas mais leves.
 '''
 
+temporário = []
 pessoas = []
-maisPeso = []
-menosPeso = []
+maiorPeso = menorPeso = 0
+#Quantidade de pessoas cadastradas
 totPessoas = 0
 while True:
-    nome = str(input('Nome: '))
-    pessoas.append(nome)
-    peso = float(input('Peso: '))
-    pessoas.append(peso)
+    temporário.append(input('Nome: '))
+    temporário.append(float(input('Peso: ')))
+    #Acima, adicionamos nome e peso em uma lista temporária
+    if len(pessoas) == 0:
+        maiorPeso = menorPeso = temporário[1]
+    else:
+        if temporário[1] > maiorPeso:
+            maiorPeso = temporário[1]
+        if temporário[1] < menorPeso:
+            menorPeso = temporário[1]
+    #Abaixo adicionamos uma lista dentro de outra lista
+    pessoas.append(temporário[:])
+    temporário.clear()
     totPessoas += 1
     continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
     if continuar == 'N':
@@ -23,5 +33,14 @@ while True:
         print('Opção inválida!')
         continuar = input('Deseja continuar? [S/N] ').upper().strip()[0]
 
-print(pessoas)
 print(f'Ao total tivemos {totPessoas} cadastradas.')
+print(f'O maior peso foi {maiorPeso}, ', end='')
+for p in pessoas:
+    if p[1] == maiorPeso:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'O menor peso foi {menorPeso}, ', end='')
+for p in pessoas:
+    if p[1] == menorPeso:
+        print(f'[{p[0]}] ', end='')
+print()
