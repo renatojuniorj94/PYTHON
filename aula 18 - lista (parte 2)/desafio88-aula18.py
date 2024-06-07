b8cbeb1fd84b5ef1346b89bf00439948e7192e2e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-07 00:01:12 -0300

    Iniciando desafio 88

--- aula 18 - lista (parte 2)/desafio88-aula18.py
+++ aula 18 - lista (parte 2)/desafio88-aula18.py
@@ -3,3 +3,8 @@ Faça um programa que ajude um jogador da mega-sena a criar palpites.
 O programa vai perguntar quantos jogos serão gerados e
 vai sortear 6 números entre 1 e 60 para cada jogo, cadastrando tudo em uma lista composta.
 '''
+
+from random import randint
+print('—' * 30)
+print(f'{'SURPRESINHA MEGA SENA':^30}')
+print('—' * 30)
