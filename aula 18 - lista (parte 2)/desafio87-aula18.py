797591ef93a409a3c11d3cb4200e13aa7a4ff0c5
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-15 00:15:16 -0300

    resolução desafio 87 (parte 1)

--- aula 18 - lista (parte 2)/desafio87-aula18.py
+++ aula 18 - lista (parte 2)/desafio87-aula18.py
@@ -20,3 +20,15 @@ print('-=' * 30)
 print(f'A soma dos valores pares é {par}')
 print(f'A soma dos valores da terceira coluna é {matriz[0][2] + matriz[1][2] + matriz[2][2]}')
 print(f'O maior valor da segunda linha é {max(matriz[1])}')
+
+#Correto! :D
+#Outra maneira de fazer...
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for linha in range(0, 3):
+    for c in range(0, 3):
+        matriz[linha][c] = int(input(f'Digite um valor na posição {[linha, c]}: '))
+for linha in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[linha][c]:^5}]', end='')
+    print()
