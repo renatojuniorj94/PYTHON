362ed36e7ed5fcdd09b782b19e09fb55e96efe22
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-17 20:16:51 -0300

    resolução desafio 87 (parte 2)

--- aula 18 - lista (parte 2)/desafio87-aula18.py
+++ aula 18 - lista (parte 2)/desafio87-aula18.py
@@ -1,12 +1,11 @@
-'''
+"""
 Aprimore o desafio anterior, mostrando no final:
 A - A soma de todos os valores pares digitados.
 B - A soma dos valores da terceira coluna.
 C - O maior valor da segunda linha.
-'''
-
+"""
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-par = colTres = 0
+par = colTres = maior = 0
 for l in range(0, 3):
     for c in range(0, 3):
         matriz[l][c] = int(input(f'Digite um valor na posição {[l, c]}: '))
@@ -24,13 +23,14 @@ print(f'O maior valor da segunda linha é {max(matriz[1])}')
 #Correto! :D
 #Outra maneira de fazer...
 
-matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+#Para somar os números da terceira coluna:
+for t in range(0, 3):
+    colTres += matriz[l][2]
+print(f'A soma da coluna três é {colTres}')
+#Maior número da segundo linha:
 for linha in range(0, 3):
-    for c in range(0, 3):
-        matriz[linha][c] = int(input(f'Digite um valor na posição {[linha, c]}: '))
-for linha in range(0, 3):
-    for c in range(0, 3):
-        print(f'[{matriz[linha][c]:^5}]', end='')
-    print()
-
-#Correto! :D
+    if linha == 0:
+        maior = matriz[1][linha]
+    elif matriz[1][linha] > maior:
+        maior = matriz[1][linha]
+print(f'O maior número da segunda linha é {maior}')
