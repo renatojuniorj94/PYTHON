944e52eaa110bc022689573ef04067a1d2361f19
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-17 20:40:34 -0300

    resolução desafio 88

--- aula 18 - lista (parte 2)/desafio88-aula18.py
+++ aula 18 - lista (parte 2)/desafio88-aula18.py
@@ -16,3 +16,31 @@ for c in range(quant_jogos):
     print(f'Jogo {c + 1}: {jogo}')
     sleep(1)
 print('-=' * 6, '< BOA SORTE! >', '-=' * 6)
+
+#Correto! :D
+#Outra maneira de fazer...
+
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'Sorteando {quant} jogos', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i + 1}: {l}')
+    sleep(1)
+print('-=' * 6, '< BOA SORTE! >', '-=' * 6)
