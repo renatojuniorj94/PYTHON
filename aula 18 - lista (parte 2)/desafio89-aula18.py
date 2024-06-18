cf0ff5ba42e069d1fb5f9bf5916587da5811d87f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-18 00:48:43 -0300

    resolução desafio 89

--- aula 18 - lista (parte 2)/desafio89-aula18.py
+++ aula 18 - lista (parte 2)/desafio89-aula18.py
@@ -35,3 +35,31 @@ while True:
     if notas == 999:
         break
     print(f'As notas de {aluno[notas][0]} são [{aluno[notas][1]}, {aluno[notas][2]}]')
+
+#Correto! :D
+#Outra maneira de fazer...
+
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota01 = float(input('Nota 1: '))
+    nota02 = float(input('Nota 2: '))
+    media = (nota01 + nota02) / 2
+    ficha.append([nome, [nota01, nota02], media])
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"Nome":<10}{"Média":8.}')
+print('—' * 26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+while True:
+    print('—' * 35)
+    opc = int(input('Mostrar notas de qual aluno? (999 interrompe): '))
+    if opc == 999:
+        print('Finalizando...')
+        break
+    if opc <= len(ficha) - 1:
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
