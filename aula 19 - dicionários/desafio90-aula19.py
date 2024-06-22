0cff93da293b2fc4c943efd5970bf08cab9c070e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-22 02:24:03 -0300

    desafio 90 (parte 1)

--- aula 19 - dicionários/desafio90-aula19.py
+++ aula 19 - dicionários/desafio90-aula19.py
@@ -2,3 +2,17 @@
 Faça um programa que leia nome e média de um aluno, guardando também
 a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
 """
+aluno = dict()
+boletim = list()
+for c in range(1):
+    aluno['nome'] = str(input('Nome: '))
+    aluno['media'] = float(input('Média: '))
+boletim.append(aluno.copy())
+for a in aluno.items():
+    print(f'Nome do aluno(a) é igual a {a}')
+    print(f'Média é igual a {m}')
+'''if m < 5:
+    print('REPROVADO!')
+else:
+    print('APROVADO!')'''
+print(boletim)
