311a454770a4f50a13ad466572bea315d159e757
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-24 00:13:30 -0300

    Desafio 91

--- aula 19 - dicionários/desafio91-aula19.py
+++ aula 19 - dicionários/desafio91-aula19.py
@@ -3,3 +3,24 @@ Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios
 Guarde esses resultados em um dicionário. No final, coloque esse dicionário
 em ordem, sabendo que o vencedor tirou o maior número no dado.
 """
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogador = {'jogador 1': randint(1, 6),
+           'jogador 2': randint(1, 6),
+           'jogador 3': randint(1, 6),
+           'jogador 4': randint(1, 6)
+           }
+ranking = dict()
+print('Valores sorteados: ')
+for k, v in jogador.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+print('—' * 30)
+print(' == RANKING DOS JOGADORES == ')
+ranking = sorted(jogador.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'  {i + 1}º lugar: {v[0]} com {v[1]}')
+    sleep(1)
+
+#Correto! :D
