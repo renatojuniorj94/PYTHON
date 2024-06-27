48c76e103f63c7cb3df1ddd9503816a10a10e84a
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-26 23:53:46 -0300

    desafio 93 (início)

--- aula 19 - dicionários/desafio93-aula19.py
+++ aula 19 - dicionários/desafio93-aula19.py
@@ -5,3 +5,8 @@ Depois vai ler a quantidade de gols feitos em cada partida.
 No final, tudo isso será guardado em um dicionário, incluindo o total
 de gols feitos durante o campeonato.
 """
+jogador = dict()
+atleta = list()
+jogador['nome'] = str(input('Digite o nome do Jogador: '))
+jogador['partidas'] = int(input('Nº de partidas: '))
+jogador['gols'] = int(input(f'Gol na partida'))
