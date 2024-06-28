1a1a56e1d75da4354e6abdb968f90fb7d924a9e5
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-27 21:19:24 -0300

    desafio 93 concluído

--- aula 19 - dicionários/desafio93-aula19.py
+++ aula 19 - dicionários/desafio93-aula19.py
@@ -5,19 +5,26 @@ Depois vai ler a quantidade de gols feitos em cada partida.
 No final, tudo isso será guardado em um dicionário, incluindo o total
 de gols feitos durante o campeonato.
 """
-#Nome
-#quantidade de partidas
-#gols por partida
-#total de gols marcados
 jogador = dict()
 atleta = list()
-total_gols = 0
+partidas = list()
+total_gols = list()
+todos_os_gols = 0
 jogador['nome'] = str(input('Digite o nome do Jogador: '))
 num_partidas = int(input('Nº de partidas: '))
 jogador['partidas'] = num_partidas
 for c in range(num_partidas):
-    jogador['gols'] = int(input(f'Gol(s) na partida {c + 1}: '))
-    total_gols += jogador['gols']
+    total_gols.append(int(input(f'Gol(s) na partida {c + 1}: ')))
+    todos_os_gols += total_gols[c]
+    jogador['gols por partida'] = total_gols[:]
+    jogador['total'] = todos_os_gols
 
-print(total_gols)
-print(jogador)
+print('-=' * 40)
+print(f'Nome do atleta: {jogador['nome']}')
+print(f'Gols por partida: {jogador['gols por partida']}')
+print(f'Fez um total de {jogador['total']} gols')
+print('-=' * 40)
+print(f'O jogador {jogador['nome']} jogou {jogador['partidas']} partidas.')
+for c in range(jogador['partidas']):
+    print(f'=>Na partida {c + 1}, fez {jogador['gols por partida'][c]} gols.'.rjust(32))
+print(f'Fez um total de {jogador['total']} gols')
