2eb0ccb0a2f912a1afbb9b67866a9d8b43dfbece
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-06-28 22:47:05 -0300

    desafio 94 (estrutura)
    Estrutura do desafio 94

--- aula 19 - dicionários/desafio94-aula19.py
+++ aula 19 - dicionários/desafio94-aula19.py
@@ -7,3 +7,24 @@ B - A média de idade do grupo
 C - Uma lista com todas as mulheres
 D - Uma lista com todas as pessoas com idade acima da média
 """
+pessoa = dict()
+lista = list()
+todas_as_idades = mulheres = 0
+while True:
+    pessoa['nome'] = str(input('Nome: '))
+    pessoa['sexo'] = str(input('Sexo [M/F]: ')).strip().upper()[0]
+    if pessoa['sexo'] == 'F':
+        mulheres += 1
+    while pessoa['sexo'] not in 'MmFf':
+        print('Opção inválida!')
+        pessoa['sexo'] = str(input('Sexo [M/F]: '))
+    pessoa['idade'] = int(input('Idade: '))
+    todas_as_idades += pessoa['idade']
+    lista.append(pessoa.copy())
+    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+print(f'Total de pessoas cadastradas: {len(lista)}')
+print(f'Média de idade: {todas_as_idades / len(lista)}')
+print(f'Total de mulheres: {mulheres}')
+print(lista)
