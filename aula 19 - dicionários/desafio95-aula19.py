bc81ddae9bb0ecc02a4736ace09af81e46fa943a
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-02 00:38:10 -0300

    desafio 95

--- aula 19 - dicionários/desafio95-aula19.py
+++ aula 19 - dicionários/desafio95-aula19.py
@@ -3,17 +3,21 @@ Aprimore o desafio 93 para que ele funcione com vários jogadores,
 incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
 """
 jogador = dict()
+lista = list()
 total_de_gols = list()
 todos_os_gols = 0
 while True:
     jogador['nome'] = str(input('Nome do jogador: '))
-    partidas = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
-    jogador['partidas'] = partidas
-    for partida in range(partidas):
-        jogador['gols'] = int(input(f'Quantos gols na partida {partida + 1}? '))
-        todos_os_gols += jogador['gols']
-        jogador['gols por partida'] = total_de_gols[:]
-    break
-print(jogador)
-print(todos_os_gols)
-print(jogador['gols por partida'])
+    jogador['partidas'] = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+    for partida in range(jogador['partidas']):
+        total_de_gols.append(int(input(f'Quantos gols na partida {partida + 1}? ')))
+        jogador['gols por partida'] = total_de_gols
+        todos_os_gols += total_de_gols[partida]
+        jogador['total'] = total_de_gols
+    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    while continuar not in 'SN':
+        print('Opção inválida!')
+        continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if continuar == 'N':
+        break
+print(lista)
