0dc369abaf56eea92d33e8ec100e3181c9276295
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-03 19:54:27 -0300

    desafio 95

--- aula 19 - dicionários/desafio95-aula19.py
+++ aula 19 - dicionários/desafio95-aula19.py
@@ -3,21 +3,31 @@ Aprimore o desafio 93 para que ele funcione com vários jogadores,
 incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador
 """
 jogador = dict()
-lista = list()
-total_de_gols = list()
-todos_os_gols = 0
+partidas = list()
+lista_jogadores = list()
 while True:
+    jogador.clear()
+    partidas.clear()
     jogador['nome'] = str(input('Nome do jogador: '))
-    jogador['partidas'] = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
-    for partida in range(jogador['partidas']):
-        total_de_gols.append(int(input(f'Quantos gols na partida {partida + 1}? ')))
-        jogador['gols por partida'] = total_de_gols
-        todos_os_gols += total_de_gols[partida]
-        jogador['total'] = total_de_gols
-    continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
-    while continuar not in 'SN':
-        print('Opção inválida!')
+    tot = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+    for c in range(0, tot):
+        partidas.append(int(input(f'Quantos gols na partida {c + 1}? ')))
+    jogador['gols'] = partidas[:]  # jogador['gols'] recebeu uma cópia de partidas.
+    jogador['total'] = sum(partidas)
+    lista_jogadores.append(jogador.copy())
+    while True:
         continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+        if continuar in 'SN':
+            break
+        print('Opção inválida!')
     if continuar == 'N':
         break
-print(lista)
+print('-=' * 30)
+print(f'{"Cod":<4}{"Nome":<15}{"gols":<15}{"total":>20}')
+print('—' * 55)
+for c, j in enumerate(lista_jogadores):
+    print(f"{c + 1:<4}{j['nome']:<15}{str(j['gols']):<15}{j['total']:>20}")
+print('—' * 55)
+print(lista_jogadores)
+while True:
+    cod_jogador = int(input('Mostrar dados de qual jogador? (999 para parar) '))
