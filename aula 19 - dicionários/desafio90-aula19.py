cdc5de1ba2878e881def8fd27562f62bbe07d2e6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-05 15:25:11 -0300

    Resolução desafio 90

--- aula 19 - dicionários/desafio90-aula19.py
+++ aula 19 - dicionários/desafio90-aula19.py
@@ -15,3 +15,20 @@ elif boletim[0]['media'] < 7:
     print('\033[1;33mRECUPERAÇÃO!\033[m')
 else:
     print('\033[1;32mAPROVADO!\033[m')
+
+#Correto! :D
+#Outra maneira de fazer...
+
+aluno = dict()
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno['nome']}: '))
+if aluno['media'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['media'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+
+print('-=' * 30)
+for key, value in aluno.items():
+    print(f' -{key} é igual a {value}')
