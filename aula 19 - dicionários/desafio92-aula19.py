cec767fbe14661a2f802536764d8c521a9e05e4c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-07 21:04:59 -0300

    Resolução desafio 92

--- aula 19 - dicionários/desafio92-aula19.py
+++ aula 19 - dicionários/desafio92-aula19.py
@@ -4,7 +4,7 @@ cadastre-os (com idade) em um dicionário se por acaso a CTPS for diferente
 de zero, o dicionário receberá também o ano de contratação e o salário.
 Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.
 """
-from datetime import datetime
+"""from datetime import datetime
 ctps = dict()
 ctps['nome'] = str(input('Nome: '))
 ctps['ano_nascimento'] = int(input('Ano de nascimento: '))
@@ -27,5 +27,20 @@ else:
     print(f'Salário: R$ {ctps['salário']}')
     print(f'A aposentadoria será com {ctps['idade'] + (ctps['contratação'] + 35) - datetime.now().year} anos de idade.')
 print(ctps)
-
+"""
 #Correto! :D
+#Outra maneira de fazer...
+
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de trabalho (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$ '))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-=' * 30)
+for k, v in dados.items():
+    print(f'    — {k} tem o valor {v}')
