a1706c58fa40eebe96bba4f6296fa5eff0caf5e4
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-07 22:16:26 -0300

    Resolução desafio 94

--- aula 19 - dicionários/desafio94-aula19.py
+++ aula 19 - dicionários/desafio94-aula19.py
@@ -7,7 +7,7 @@ B - A média de idade do grupo
 C - Uma lista com todas as mulheres
 D - Uma lista com todas as pessoas com idade acima da média
 """
-pessoa = dict()
+"""pessoa = dict()
 lista = list()
 todas_as_idades = mulheres = 0
 while True:
@@ -34,4 +34,44 @@ print(f'Total de mulheres: {mulheres}')
 print('Pessoas com idade acima da média:')
 for p in lista:
     if p['idade'] >= media:
-        print(f'Nome: {p['nome']}, Idade: {p['idade']}')
+        print(f'Nome: {p['nome']}, Idade: {p['idade']}')"""
+
+#Correto! :D
+#Outra maneira de fazer...
+
+pessoa = dict()
+galera = list()
+soma = media = 0
+while True:
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: '))
+    while True:
+        pessoa['sexo'] = str(input('Sexo: [M/F] ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    galera.append(pessoa.copy())
+    while True:
+        resp = str(input('Quer continuar? [S/N] ')).upper()[0]
+        if resp in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if resp == 'N':
+        break
+print('-=' * 30)
+print(f'Ao todo temos {len(galera)} pessoas cadastradas.')
+media = soma / len(galera)
+print(f'A média de idade é de {media} anos.')
+print('As mulheres cadastradas foram:')
+for p in galera:
+    if p['sexo'] in 'Ff':
+        print(f'{p['nome']}')
+print('Lista das pessoas acima da média:')
+for p in galera:
+    if p['idade'] >= media:
+        for k, v in p.items():
+            print(f'{k} = {v}; ', end='')
+        print()
+print('<< ENCERRADO >>')
