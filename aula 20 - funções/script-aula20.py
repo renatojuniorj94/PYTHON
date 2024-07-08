ae3e052dff045ee82d8426992fee67610004c5e5
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-08 00:57:43 -0300

    Aula 20 - funções (parte 1)

---
+++ aula 20 - funções/script-aula20.py
@@ -0,0 +1,24 @@
+def mensagem(msg):
+    print('—' * 30)
+    print(msg)
+    print('—' * 30)
+
+
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma entre A + B = {s}')
+
+
+#Programa principal
+#Função mensagem()
+mensagem('COPA LIBERTADORES')
+mensagem('CAMPEONATO BRASILEIRO')
+mensagem('COPA DO BRASIL')
+
+#Função soma()
+soma(a=4, b=5)
+soma(8, 9)
+soma(2, 1)
+
+#Paramos a aula com 27 minutos
