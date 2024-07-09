bfca48883c6886df468667938ca71db906a2d014
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-09 14:29:28 -0300

    desafio 97

--- aula 20 - funções/desafio97-aula20.py
+++ aula 20 - funções/desafio97-aula20.py
@@ -8,3 +8,21 @@ Saída:
 Olá, mundo!
 ~~~~~~~~~~~
 """
+#ler = len(escreva)
+
+
+def escreva(msg):
+    for c in msg:
+        ler = len(msg)
+    print('~' * ler)
+    print(msg)
+    print('~' * ler)
+
+
+escreva('  Copa Libertadores  ')
+escreva('  Brasileirão  ')
+escreva('  Copa do Brasil  ')
+escreva('  CLUBE DE REGATAS DO FLAMENGO  ')
+escreva('  CURSO DE PYTHON NO YOUTUBE (CURSO EM VÍDEO)  ')
+
+#Correto! :D
