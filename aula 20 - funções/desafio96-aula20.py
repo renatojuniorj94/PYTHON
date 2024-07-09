0d65c8cf9a6b107d7ee8539ba0aa33e4a589fc85
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-09 14:10:13 -0300

    desafio 96

--- aula 20 - funções/desafio96-aula20.py
+++ aula 20 - funções/desafio96-aula20.py
@@ -2,3 +2,19 @@
 Faça um programa que tenha uma função chamada área(), que receba as dimensões de um
 terreno retangular(largura e comprimento) e mostre a área do terreno.
 """
+texto = 'Controle de terrenos'
+quant_caracteres = len(texto)
+print(texto)
+print('—' * quant_caracteres)
+largura = float(input('LARGURA (m): '))
+comprimento = float(input('COMPRIMENTO (m): '))
+
+
+def área(a, b):
+    s = a * b
+    print(f'A área do seu terreno {a} x {b} é de {s}m².')
+
+
+área(largura, comprimento)
+
+#Corrreto! :D
