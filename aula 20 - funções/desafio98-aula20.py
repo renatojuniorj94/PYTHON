663c3fcb21aa3f28d42e94993ad8621e96de0c8a
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-11 16:38:57 -0300

    desafio 98
    Adicionamos o link "Definição de funções" em Python, na nossa documentação pessoal.

--- aula 20 - funções/desafio98-aula20.py
+++ aula 20 - funções/desafio98-aula20.py
@@ -6,3 +6,26 @@ A) De 1 até 10, de 1 em 1
 B) De 10 até 0, de 2 em 2
 C) Uma contagem personalizada.
 """
+from time import sleep
+
+
+def contador(i, f, p):
+    print('-=' * 30)
+    print(f'Contagem de {i} até {f} de {p} em {p}.')
+    if f < i:
+        p = -p
+    for c in range(i, f + (1 if p > 0 else -1), p):
+        print(c, end=' ')
+        sleep(0.5)
+    print('FIM!')
+
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+
+print('-=' * 30)
+print('Agora é sua vez de personalizar a contagem!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
