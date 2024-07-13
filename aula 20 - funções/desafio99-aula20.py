cd64572cd14db1642e4b55204d5d6f68e772fe92
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-13 00:04:53 -0300

    desafio 99
    - Definindo parâmetros da def maior()

--- aula 20 - funções/desafio99-aula20.py
+++ aula 20 - funções/desafio99-aula20.py
@@ -4,3 +4,16 @@ com valores inteiros.
 Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
 """
 from random import randint
+mini = randint(1, 100)
+maxi = randint(1, 100)
+
+
+def maior(*num):
+    tamanho = len(num)
+    print('-=' * 30)
+    print('Analisando os valores passados...')
+    print(f'{num} foram informados {tamanho} valores ao todo.')
+    print(f'O maior valor informado foi .')
+
+
+maior(mini, maxi)
