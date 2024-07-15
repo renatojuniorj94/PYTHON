5f000cc34ce2c47a3ab9f473d5ddc8c8552d4e5a
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-15 15:25:15 -0300

    Resolução desafio 100

--- aula 20 - funções/desafio100-aula20.py
+++ aula 20 - funções/desafio100-aula20.py
@@ -28,3 +28,29 @@ def somaPar():
 
 sorteia()
 somaPar()
+
+#Correto! :D
+#Outra maneira de fazer...
+
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for contador in range(0, 5):
+        n = randint(1, 10)
+        lista.append(n)
+        print(f'{n} ', end='')
+        sleep(0.3)
+    print('PRONTO!')
+
+
+def somarPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+
+números = list()
+sorteia(números)
+somarPar(números)
