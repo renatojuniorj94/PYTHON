da417aed49c79a91e3fc37cef756f70eebdf770d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-16 23:57:30 -0300

    desafio 101

--- aula 21 - funções/desafio101-aula21.py
+++ aula 21 - funções/desafio101-aula21.py
@@ -3,3 +3,18 @@ Crie um programa que tenha uma função voto() que vai receber como parâmetro o
 de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem
 voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 """
+
+
+def voto(nasc):
+    nasc = int(input('Ano de nascimento: '))
+    if nasc < 16:
+        print('Não pode votar')
+    elif nasc == 17:
+        print('Voto opcional')
+    elif 18 <= nasc < 63:
+        print('Voto obrigatório!')
+    else:
+        print('Voto opcional')
+
+
+voto()
