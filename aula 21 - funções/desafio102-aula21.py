cc93e582e52aed390197f68d76bd757a284c7372
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-18 22:34:42 -0300

    desafio 102

--- aula 21 - funções/desafio102-aula21.py
+++ aula 21 - funções/desafio102-aula21.py
@@ -3,3 +3,6 @@ Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
 O primeiro que indique o número a calcular e o outro chamado show, que será um valor
 lógico(opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 """
+
+
+def show()
