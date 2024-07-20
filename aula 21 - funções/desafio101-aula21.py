2456e4374fb6136e935d55e7b9a16f1af85a244b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-20 01:12:46 -0300

    Resolução desafio 101

--- aula 21 - funções/desafio101-aula21.py
+++ aula 21 - funções/desafio101-aula21.py
@@ -3,14 +3,17 @@ Crie um programa que tenha uma função voto() que vai receber como parâmetro o
 de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem
 voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 """
-from datetime import date
+"""from datetime import date
 
 
 def voto():
     global idade
     anoNasc = int(input('Ano de nascimento: '))
     idade = date.today().year - anoNasc
-    print(f'Com {idade} anos: ', end='')
+    if idade == 1:
+        print(f'Com {idade} ano: ', end='')
+    else:
+        print(f'Com {idade} anos: ', end='')
     if idade < 16:
         print('NÃO PODE VOTAR.')
     elif idade < 18:
@@ -21,4 +24,23 @@ def voto():
         print('NÃO PRECISAR VOTAR')
 
 
-voto()
+voto()"""
+
+#Correto! :D
+#Outra maneira de fazer...
+
+
+def voto(ano):
+    from datetime import date  # Escopo de função
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO.'
+
+
+nasc = int(input('Em que ano você nasceu? '))
+print(voto(nasc))
