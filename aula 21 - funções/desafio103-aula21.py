f9f0d609922b94e1a0c16b204f63d60e83063f12
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-21 01:42:05 -0300

    desafio 103

--- aula 21 - funções/desafio103-aula21.py
+++ aula 21 - funções/desafio103-aula21.py
@@ -5,3 +5,12 @@ O nome de um jogador e quantos gols ele marcou.
 O programa deverá ser capaz de mostrar a ficha do jogador, mesmo que algum dado
 não tenha sido informado corretamente.
 """
+
+
+def ficha(jogador=0, quant_gols=0):
+    jogador = str(input('Nome do jogador: '))
+    quant_gols = int(input('Número de gols: '))
+    print(f'O jogador {jogador} fez {quant_gols}gol(s) no campeonato.')
+
+
+ficha()
