60f0185d1a12808eb613a1d78f2ea5a815446b6d
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-21 21:54:04 -0300

    Resolução desafio 103

--- aula 21 - funções/desafio103-aula21.py
+++ aula 21 - funções/desafio103-aula21.py
@@ -7,14 +7,20 @@ não tenha sido informado corretamente.
 """
 
 
-def ficha(jogador, quant_gols):
-    if jogador == '':
-        jogador = '<desconhecido>'
-    if quant_gols == '':
-        quant_gols = 0
-    print(f'O jogador {jogador} fez {quant_gols} gol(s) no campeonato.')
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} marcou {gols} gol(s).')
 
 
-jogador = str(input('Nome do jogador: '))
-quant_gols = int(input('Número de gols: '))
-ficha(jogador, quant_gols)
+n = str(input('Nome do jogador: '))
+g = str(input(f'Quantos gols {n} marcou? '))
+if g.isnumeric():  # Se 'g' receber um dado numérico: 'g' vira int()
+    g = int(g)
+else:  # Se não receber um dado como string, ele recebe '0' como padrão.
+    g = 0
+if n.strip() == '':  # Se 'n' receber um dado em branco, a função ficha vai receber 'g' em 'gols' e
+    #'nome' recebe a resposta padrão ('<desconhecido>')
+    ficha(gols=g)
+else:
+    ficha(n, g)
+
+#Desafio feito na aula de resolução.
