52254a6d537b247f8fae7e0f8f5cc2d0742bbefd
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-29 15:28:31 -0300

    Resolução desafio 105

--- aula 21 - funções/desafio105-aula21.py
+++ aula 21 - funções/desafio105-aula21.py
@@ -10,7 +10,7 @@ Adicione também as docstrings da função.
 """
 
 
-def notas(*n, sit=False):
+'''def notas(*n, sit=False):
     """
     —> Função para analisar notas e situações de vários alunos.
     :param n: Pode receber uma quantidade indeterminada de números.
@@ -33,8 +33,38 @@ def notas(*n, sit=False):
             itens['situação'] = 'Boa'
     else:
         ''
-
     print(itens)
 
 
-notas(5, 8, 7, 4.2, 3.2, 2.2, sit=True)
+notas(5, 8, 7, 4.2, 3.2, 2.2, sit=True)'''
+
+#Correto! :D
+#Outra maneira de fazer...
+
+
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: Uma ou mais notas dos alunos (aceita várias)
+    :param sit: Valor opcional, indicando se deve ou não adicionar a situação
+    :return: Dicionario com várias informações sobre a situação da turma.
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['média'] = sum(n) / len(n)
+    if sit:
+        if r['média'] >= 7:
+            r['situação'] = 'BOA'
+        elif r['média'] >= 5:
+            r['situação'] = 'RAZOÁVEL'
+        else:
+            r['situação'] = 'RUIM'
+    return r
+
+
+#Programa principal
+resp = notas(5.5, 2.5, 1.5, sit=True)
+print(resp)
+help(notas)
