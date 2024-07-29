809efca9140b25dc2ed4c0d0cb460d8503d23d4c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-29 17:18:25 -0300

    Resolução desafio 106

--- aula 21 - funções/desafio106-aula21.py
+++ aula 21 - funções/desafio106-aula21.py
@@ -6,11 +6,10 @@ OBS: Use cores.
 """
 
 
-def ajuda():
+"""def ajuda():
     while True:
         from time import sleep
         msg_1 = 'SISTEMA DE AJUDA PyHELP'
-
         print('\033[1;42m~' * len(msg_1))
         print(msg_1)
         print('~' * len(msg_1))
@@ -30,4 +29,47 @@ def ajuda():
         help(resp)
 
 
-ajuda()
+ajuda()"""
+
+#Correto! :D
+#Outra maneira de fazer...
+
+from time import sleep
+c = ('\033[m',         # 0 - sem cores
+     '\033[0;30;41m',  # 1 - vermelho
+     '\033[0;30;42m',  # 2 - verde
+     '\033[0;30;43m',  # 3 - amarelo
+     '\033[0;30;44m',  # 4 - azul
+     '\033[0;30;45m',  # 5 - roxo
+     '\033[7;97m'      # 6 - branco
+     )
+
+
+def ajuda(com):
+    título(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def título(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+
+#programa principal
+comando = ''
+while True:
+    título('SISTEMA DE AJUDA PyHELP', 2)
+    comando = str(input('Função ou biblioteca > '))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+título('ATÉ LOGO!', 1)
