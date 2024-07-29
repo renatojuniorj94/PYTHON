f51d842395f94d5bd04d8d7a881d9c11ca69b38b
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-29 16:39:24 -0300

    desafio 106 (código final)

--- aula 21 - funções/desafio106-aula21.py
+++ aula 21 - funções/desafio106-aula21.py
@@ -4,3 +4,30 @@ o comando e o manual vai aparecer. Quando o usuário digitar a palavra 'FIM', o 
 se encerrará.
 OBS: Use cores.
 """
+
+
+def ajuda():
+    while True:
+        from time import sleep
+        msg_1 = 'SISTEMA DE AJUDA PyHELP'
+
+        print('\033[1;42m~' * len(msg_1))
+        print(msg_1)
+        print('~' * len(msg_1))
+        resp = str(input('\033[mFunção ou biblioteca > ')).lower()
+        if resp == 'fim':
+            fim = 'ATÉ LOGO!'
+            print('\033[1;41m~' * len(fim))
+            print(fim)
+            print('~' * len(fim))
+            break
+        msg_2 = f"Acessando o manual do comando '{resp}'"
+        print('\033[1;44m~' * len(msg_2))
+        print(msg_2)
+        print('~' * len(msg_2))
+        sleep(1)
+        print('\033[1;30;107m')
+        help(resp)
+
+
+ajuda()
