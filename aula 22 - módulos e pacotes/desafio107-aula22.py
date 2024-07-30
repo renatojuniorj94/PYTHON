db677dd4e6279f2e9127b9e173bdd8aad5cb4a7e
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-30 01:11:28 -0300

    Enunciado dos desafios 107 ao 112

---
+++ aula 22 - módulos e pacotes/desafio107-aula22.py
@@ -0,0 +1,6 @@
+"""
+Crie um módulo chamado moeda.py que tenha as funções incorporadas
+aumentar(), diminuir(), dobra() e metade().
+Faça também um programa que importe esse módulo e
+use alguma dessas funções.
+"""
