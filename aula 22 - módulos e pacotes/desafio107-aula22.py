1f5e3ece9076ac1e1294f4ea12c73a3587d87e5f
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-07-31 15:17:34 -0300

    desafio 107

--- aula 22 - módulos e pacotes/desafio107-aula22.py
+++ aula 22 - módulos e pacotes/desafio107-aula22.py
@@ -4,3 +4,9 @@ aumentar(), diminuir(), dobra() e metade().
 Faça também um programa que importe esse módulo e
 use alguma dessas funções.
 """
+from ex107 import moedas
+preço = float(input('Digite o preço: '))
+print(f'A metade de {preço} é {moedas.metade(preço)}')
+print(f'O dobro de {preço} é {moedas.dobra(preço)}')
+print(f'Aumentando 10% temos {moedas.aumentar(preço)}')
+print(f'Reduzindo 13% temos {moedas.diminuir(preço)}')
---
+++ aula 22 - módulos e pacotes/ex107/moedas.py
@@ -0,0 +1,14 @@
+def aumentar(num):
+    return num + ((num * 10) / 100)
+
+
+def diminuir(num):
+    return num - ((num * 13) / 100)
+
+
+def dobra(num):
+    return num * 2
+
+
+def metade(num):
+    return num / 2
