c0b3aad512c923357716b728127c81e550b75ec6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-02 15:54:56 -0300

    desafio 108

--- aula 22 - módulos e pacotes/desafio108-aula22.py
+++ aula 22 - módulos e pacotes/desafio108-aula22.py
@@ -3,6 +3,9 @@ Adapte o código do desafio 107, criando uma função
 adicional chamada moeda() que consiga mostrar os valores
 como um valor monetário formatado.
 """
-
-
-def moeda():
+from ex107 import moedas
+preço = float(input('Digite o preço: '))
+print(f'A metade de {preço} é {moedas.metade(preço)}')
+print(f'O dobro de {preço} é {moedas.dobra(preço)}')
+print(f'Aumentando 10% temos {moedas.aumentar(preço)}')
+print(f'Reduzindo 13% temos {moedas.diminuir(preço)}')
