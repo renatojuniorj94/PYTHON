f72ee74f4e4dcd9d058098c19b7a23de4337eb0a
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-08 20:55:53 -0300

    desafio 110

--- aula 22 - módulos e pacotes/desafio110-aula22.py
+++ aula 22 - módulos e pacotes/desafio110-aula22.py
@@ -3,3 +3,6 @@ Adicione ao módulo moeda.py criado nos desafios anteriores,
 uma função chamada resumo(), que mostre na tela algumas
 informações geradas pelas funções que já temos no módulo criado até aqui.
 """
+from ex110 import moedas
+p = float(input('Digite o preço: R$ '))
+moedas.resumo(p, 20, 12)
