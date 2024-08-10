842598136304857e47abfd95b32faa492fe6277c
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-10 01:03:49 -0300

    Código final desafio 111

--- aula 22 - módulos e pacotes/desafio111-aula22.py
+++ aula 22 - módulos e pacotes/desafio111-aula22.py
@@ -4,3 +4,6 @@ módulos chamados moeda e dado.
 Transfira todas as funções utilizadas nos desafios 107,
 108 e 109 para o primeiro pacote e mantenha tudo funcionando.
 """
+from ex111.utilidadescev import moeda
+p = float(input('Digite o preço: R$ '))
+moeda.resumo(p, 20, 12)
