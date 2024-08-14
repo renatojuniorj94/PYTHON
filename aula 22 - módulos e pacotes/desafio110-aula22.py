6ee0e5c780eebc8e70dc5b626262f6a2ecc174d6
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-14 20:42:06 -0300

    Resolução desafio 110

--- aula 22 - módulos e pacotes/desafio110-aula22.py
+++ aula 22 - módulos e pacotes/desafio110-aula22.py
@@ -6,3 +6,6 @@ informações geradas pelas funções que já temos no módulo criado até aqui.
 from ex110 import moedas
 p = float(input('Digite o preço: R$ '))
 moedas.resumo(p, 20, 12)
+
+#Correto! :D
+#Outra maneira fazer está no pacote ex110 modulo moedas.py
