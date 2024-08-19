0b8dfd112ea4f82c1c3c8c18a40b55952d6c9430
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-18 23:52:14 -0300

    Atualizando função pessoasCadastradas()

--- aula 23 - tratamento de erros/ex115/__init__.py
+++ aula 23 - tratamento de erros/ex115/__init__.py
@@ -10,17 +10,16 @@ def menu():
 
 
 def pessoasCadastradas():
-    pessoas = [
-        {'nome': 'Ana Paula Vieira', 'idade': 32},
-        {'nome': 'Claudio Mendonça', 'idade': 18},
-        {'nome': 'Gustavo Guanabara', 'idade': 41},
-        {'nome': 'Maria Clara Peixoto', 'idade': 65},
-        {'nome': 'Mauricio Souza', 'idade': 19},
-        {'nome': 'Nilce Pedrosa', 'idade': 43},
-        {'nome': 'Pedro Gonçalves', 'idade': 18},
-        {'nome': 'Rafael Albuquerque', 'idade': 38},
-        {'nome': 'Renata Soares', 'idade': 13},
-    ]
+    lista = []
+    pessoas = {'nome': 'Ana Paula Vieira', 'idade': 32},
+    {'nome': 'Claudio Mendonça', 'idade': 18},
+    {'nome': 'Gustavo Guanabara', 'idade': 41},
+    {'nome': 'Maria Clara Peixoto', 'idade': 65},
+    {'nome': 'Mauricio Souza', 'idade': 19},
+    {'nome': 'Nilce Pedrosa', 'idade': 43},
+    {'nome': 'Pedro Gonçalves', 'idade': 18},
+    {'nome': 'Rafael Albuquerque', 'idade': 38},
+    {'nome': 'Renata Soares', 'idade': 13}
     print('—' * 40)
     print('PESSOAS CADASTRADAS'.center(40))
     print('—' * 40)
@@ -28,4 +27,14 @@ def pessoasCadastradas():
         print(f'{p['nome']:<25}{p['idade']} anos')
 
 
-pessoasCadastradas()
+def novoCadastro():
+    print('—' * 40)
+    print('NOVO CADASTRO'.center(40))
+    print('—' * 40)
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    print(f'Registro de {nome} adicionado')
+
+
+
+novoCadastro()
