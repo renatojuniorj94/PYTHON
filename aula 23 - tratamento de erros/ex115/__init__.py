6a0588556cdb1fee9cd7402890c55486048569b3
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-19 00:51:19 -0300

    desafio 115
    Funções finalizadas

--- aula 23 - tratamento de erros/ex115/__init__.py
+++ aula 23 - tratamento de erros/ex115/__init__.py
@@ -1,17 +1,5 @@
-def menu():
-    print('—' * 40)
-    print('MENU PRINCIPAL'.center(40))
-    print('—' * 40)
-    print('1 - Ver pessoas cadastradas')
-    print('2 - Cadastrar nova pessoa')
-    print('3 - Sair do sistema')
-    print('—' * 40)
-    opção = int(input('Sua opção: '))
-
-
-def pessoasCadastradas():
-    lista = []
-    pessoas = {'nome': 'Ana Paula Vieira', 'idade': 32},
+pessoas = [
+    {'nome': 'Ana Paula Vieira', 'idade': 32},
     {'nome': 'Claudio Mendonça', 'idade': 18},
     {'nome': 'Gustavo Guanabara', 'idade': 41},
     {'nome': 'Maria Clara Peixoto', 'idade': 65},
@@ -20,6 +8,32 @@ def pessoasCadastradas():
     {'nome': 'Pedro Gonçalves', 'idade': 18},
     {'nome': 'Rafael Albuquerque', 'idade': 38},
     {'nome': 'Renata Soares', 'idade': 13}
+    ]
+
+
+def menu():
+    from time import sleep
+    while True:
+        print('—' * 40)
+        print('MENU PRINCIPAL'.center(40))
+        print('—' * 40)
+        print('1 - Ver pessoas cadastradas')
+        print('2 - Cadastrar nova pessoa')
+        print('3 - Sair do sistema')
+        print('—' * 40)
+        opção = int(input('Sua opção: '))
+        if opção == 1:
+            pessoasCadastradas()
+            sleep(3)
+        elif opção == 2:
+            novoCadastro()
+            sleep(3)
+        else:
+            break
+
+
+def pessoasCadastradas():
+    from time import sleep
     print('—' * 40)
     print('PESSOAS CADASTRADAS'.center(40))
     print('—' * 40)
@@ -33,8 +47,5 @@ def novoCadastro():
     print('—' * 40)
     nome = str(input('Nome: '))
     idade = int(input('Idade: '))
+    pessoas.append({'nome': nome, 'idade': idade})
     print(f'Registro de {nome} adicionado')
-
-
-
-novoCadastro()
