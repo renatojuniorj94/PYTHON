5deda2469d38fa7e5f88e02700c0c79c339f0351
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-20 23:32:41 -0300

    Resolução desafio 113

--- aula 23 - tratamento de erros/desafio113-aula23.py
+++ aula 23 - tratamento de erros/desafio113-aula23.py
@@ -5,7 +5,7 @@ Aproveite e crie também uma função leiaFloat() com a mesma funcionalidade.
 """
 
 
-def leiaInt(msg):
+"""def leiaInt(msg):
     ok = False
     valor = 0
     while True:
@@ -40,4 +40,40 @@ def leiaFloat(msg):
 
 inteiro = leiaInt('Digite um inteiro: ')
 real = leiaFloat('Digite um número real: ')
-print(f'O valor inteiro digitado foi {inteiro} e o real foi {real}')
+print(f'O valor inteiro digitado foi {inteiro} e o real foi {real}')"""
+
+#Correto! :D
+#Outra maneira de fazer...
+
+
+def leiaInt(msg):
+    while True:
+        try:
+            n = int(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: Por favor, digite um número inteiro válido\033[m')
+            continue
+        except(KeyboardInterrupt):
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+def leiaFloat(msg):
+    while True:
+        try:
+            n = float(input(msg))
+        except (ValueError, TypeError):
+            print('\033[31mERRO: Por favor, digite um número real válido.\033[m')
+            continue
+        except KeyboardInterrupt:
+            print('\n\033[31mUsuário preferiu não digitar esse número.\033[m')
+            return 0
+        else:
+            return n
+
+
+n1 = leiaInt('Digite um número inteiro: ')
+n2 = leiaFloat('Digite um número real: ')
+print(f'O valor  inteiro digitado foi {n1} e o real foi {n2}')
