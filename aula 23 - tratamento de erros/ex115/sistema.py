b844beb691fc5ea7e79e03ca4e506961ef001582
renatojuniorj94 <134095376+renatojuniorj94@users.noreply.github.com>
2024-08-22 01:22:37 -0300

    Resolução desafio 115 - parte 1

---
+++ aula 23 - tratamento de erros/ex115/sistema.py
@@ -0,0 +1,15 @@
+from lib.interface import *
+from time import sleep
+#cabeçalho('SISTEMA ARQUIVO v1.0')
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova pessoa', 'Sair do sistema'])
+    if resposta == 1:
+        cabeçalho('Opção 1')
+    elif resposta == 2:
+        cabeçalho('Opção 2')
+    elif resposta == 3:
+        cabeçalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
+        sleep(2)
